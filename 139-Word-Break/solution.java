8f591a6e254e1fca6e285a09273fd74949008e97
JimmyXie123 <xiezj12345@gmail.com>
2016-05-26 15:30:38 -0400

    [139. Word Break][Runtime Error]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -11,7 +11,7 @@ public class Solution {
         
         for(int i=1; i<f.length; i++){
             for(int j=0; j<i; j++){
-                if(f[j]==true&&wordDict.contains(s.substring(j, i-1))){
+                if(f[j]==true&&wordDict.contains(s.substring(j, i))){
                     f[i] = true;
                     break;
                 }
@@ -20,7 +20,7 @@ public class Solution {
         }
         
         System.out.println(f[4]);
-        
+        System.out.println(s.substring(0,3));
         return f[s.length()];
     }
 }
