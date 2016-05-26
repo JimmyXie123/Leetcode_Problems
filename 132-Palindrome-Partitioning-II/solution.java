4a46c3a0458d905f9cce691aaaac02347c07fc7d
JimmyXie123 <xiezj12345@gmail.com>
2016-05-26 14:31:25 -0400

    [132. Palindrome Partitioning II][Compile Error]committed by LeetCode Extension

---
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -0,0 +1,45 @@
+public class Solution {
+    private boolean[][] isPalindrome(String s){
+        boolean isPalindrome[][] = new boolean[s.length()][s.length()];
+        for(int i=0; i<s.length(); i++){
+            isPalindrome[i][i] = true;
+        }
+        
+        for(int i=0; i<s.length()-1; i++){
+            isPalindrome[i][i+1] = (s.charAt(i)==s.charAt(i+1));
+        }
+        
+        for(int length=2; length<s.length(); length++){
+            for(int i=0; i+length<s.length(); i++){
+               isPalindrome[i][i+length] = isPalindrome[i+1][i+length-1]&&(s.charAt(i)==s.charAt(i+length)); 
+            }
+        }
+        
+        return isPalindrome;
+    }
+    public int minCut(String s) {
+        if(s==null||s.length()==0){
+            return 0;
+        }
+        
+        boolean test[][] = isPalindrome(s);
+        
+        int f[] = new int[s.length+1];
+        for(int i=0; i<f.length; i++){
+            f[i] = i;
+        }
+        
+        int best = Integer.MAX_VALUE;
+        for(int i=1; i<f.length; i++){
+            for(int j=0; j<i; j++){
+                if(isPalindrome[j][i-1]){
+                    f[i] = Math.min(f[i], f[j]+1);
+                }
+            }
+        }
+        
+        return f[s.length()]-1;
+        
+        
+    }
+}
