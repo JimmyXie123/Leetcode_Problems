94376351cb3fc22df4c71155afaaafeeeb629d76
JimmyXie123 <xiezj12345@gmail.com>
2016-06-02 14:05:09 -0400

    [257. Binary Tree Paths][Compile Error]committed by LeetCode Extension

---
+++ 257-Binary-Tree-Paths/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList();
+        String str = new String();
+        int flag = 0;
+        traverse(root, str, result, flag);
+        return result;
+    }
+    
+    private void traverse(TreeNode node, String str, List<String> result, int flag){
+        if(node==null){
+            return;
+        }
+        if(flag==1){
+            str = str+"->"+node.val;
+        }else{
+            str = str+node.val;
+            flag = 1;
+        }
+        if(node.left==null&&node.right==null){
+            result.add(new String(str));
+        }
+        traverse(node.left, str, result);
+        traverse(node.right, str, result);
+        str = str.substring(0, str.length());
+    }
+}
