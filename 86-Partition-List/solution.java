3dd3e3a3d6a506dd214cc23c24f7042347197344
JimmyXie123 <xiezj12345@gmail.com>
2016-06-05 15:48:23 -0400

    [86. Partition List][Accepted]committed by LeetCode Extension

--- 86-Partition-List/solution.java
+++ 86-Partition-List/solution.java
@@ -10,21 +10,19 @@ public class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode dummy_small = new ListNode(0);
         ListNode dummy_big = new ListNode(0);
-        ListNode temp =new ListNode(0);
+        //ListNode temp = new ListNode(0);
         dummy_small.next = head;
         dummy_big.next = head;
-        temp.next = head;
+        //temp.next = head;
         ListNode pointer1 = dummy_small;
-        ListNode pointer2 = dummy_big;
-        head = temp;
+        //ListNode pointer2 = dummy_big;//-------------------思考
+        head = dummy_big;
         while(head.next!=null){
             if(head.next.val<x){
                 pointer1.next = head.next;
                 pointer1 = pointer1.next;
                 head.next = head.next.next;
             }else{
-                pointer2.next = head.next;
-                pointer2 = pointer2.next;
                 head = head.next;
             }
         }
