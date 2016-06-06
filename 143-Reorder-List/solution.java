31cfc6721d6395183a0b8141da77e7e4f5fabf14
JimmyXie123 <xiezj12345@gmail.com>
2016-06-05 22:18:41 -0400

    [143. Reorder List][Compile Error]committed by LeetCode Extension

---
+++ 143-Reorder-List/solution.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null){
+            return;
+        }
+        ListNode mid = findMiddle(head);
+        ListNode back = reverse(mid.next);
+        mid.next = null;
+        ListNode result = merge(head, back);
+        return result;
+    }
+    
+    private ListNode findMiddle(ListNode head){
+        ListNode slow=head, fast=head.next;
+        while(fast!=null&&fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    
+    private ListNode reverse(ListNode head){
+        ListNode prev = null;
+        while(head!=null){
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+    
+    private ListNode merge(ListNode node1, ListNode node2){
+        ListNode dummy = new ListNode(0);
+        ListNode pointer = dummy;
+        while(node1!=null&&node2!=null){
+            pointer.next = node1;
+            node1 = node1.next;
+            pointer = pointer.next;
+            pointer.next = node2;
+            node2 = node2.next;
+            pointer = pointer.next;
+        }
+        while(node1!=null&&node2==null){
+            pointer.next = node1;
+            node1 = node1.next;
+            pointer = pointer.next;
+        }
+        while(node1==null&&node2!=null){
+            pointer.next = node2;
+            node2 = node2.next;
+            pointer = pointer.next;
+        }
+        return dummy.next;
+    }
+}
