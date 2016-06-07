b7fe114ac024df5bc1ebf6159262a6fa145b8a60
JimmyXie123 <xiezj12345@gmail.com>
2016-06-07 14:11:15 -0400

    [86. Partition List][Time Limit Exceeded]committed by LeetCode Extension

--- 86-Partition-List/solution.java
+++ 86-Partition-List/solution.java
@@ -8,25 +8,23 @@
  */
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode dummy_small = new ListNode(0);
-        ListNode dummy_big = new ListNode(0);
+        ListNode leftDummy = new ListNode(0), leftTail = leftDummy;
+        ListNode rightDummy = new ListNode(0), rightTail = rightDummy;
         //ListNode temp = new ListNode(0);
-        dummy_small.next = head;
-        dummy_big.next = head;
         //temp.next = head;
-        ListNode pointer1 = dummy_small;
         //ListNode pointer2 = dummy_big;//-------------------思考
-        head = dummy_big;
-        while(head.next!=null){
-            if(head.next.val<x){
-                pointer1.next = head.next;
-                pointer1 = pointer1.next;
-                head.next = head.next.next;
+        while(head!=null){
+            if(head.val<x){
+                leftTail.next = head;
+                leftTail = leftTail.next;
+                head = head.next;
             }else{
+                rightTail.next = head;
+                rightTail = rightTail.next;
                 head = head.next;
             }
         }
-        pointer1.next = dummy_big.next;
-        return dummy_small.next;
+        leftTail.next = rightDummy.next; 
+        return leftDummy.next;
     }
 }
