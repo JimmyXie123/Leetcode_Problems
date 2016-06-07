94379c694238f0549be82fcff6bde646be9b374c
JimmyXie123 <xiezj12345@gmail.com>
2016-06-07 14:19:36 -0400

    [141. Linked List Cycle][Accepted]committed by LeetCode Extension

--- 141-Linked-List-Cycle/solution.java
+++ 141-Linked-List-Cycle/solution.java
@@ -11,6 +11,9 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
         ListNode slow = head, fast = head.next;
         while(fast!=null&&fast.next!=null){
             if(slow==fast){
