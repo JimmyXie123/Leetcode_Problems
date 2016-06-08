486728dc7bc26fa6a8152efc46e74de059eb181d
JimmyXie123 <xiezj12345@gmail.com>
2016-06-08 16:35:02 -0400

    [75. Sort Colors][Runtime Error]committed by LeetCode Extension

---
+++ 75-Sort-Colors/solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        //int[] result = new int[nums.length];
+        int mid = nums.length/2;
+        int[] left = new int[nums.length/2];
+        int[] right = new int[nums.length-nums.length/2];
+        for(int i=0; i<nums.length/2; i++){
+            left[i] = nums[i];
+        }
+        for(int i=nums.length/2; i<nums.length; i++){
+            right[i-nums.length/2] = nums[i];
+        }
+        sortColors(left);
+        sortColors(right);
+        nums = merge(left, right);
+        
+    }
+    
+    private int[] merge(int[] left, int[] right){
+        int[] result = new int[left.length+right.length];
+        int i=0, j=0, l=0;
+        while(i<left.length&&j<right.length){
+            if(left[i]<=right[j]){
+                result[l] = left[i];
+                l++; 
+                i++;
+            }else{
+                result[l] = right[j];
+                l++;
+                j++;
+            }
+        }
+        while(i>=left.length&&j<right.length){
+            result[l++] = right[j++];
+        }
+        while(i<left.length&&j>=right.length){
+            result[l++] = left[i++];
+        }
+        return result;
+    }
+}
