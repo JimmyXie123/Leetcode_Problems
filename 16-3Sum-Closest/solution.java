a11b38d33cd28d150109fe916360c62e991f1068
JimmyXie123 <xiezj12345@gmail.com>
2016-06-08 14:58:14 -0400

    [16. 3Sum Closest][Compile Error]committed by LeetCode Extension

---
+++ 16-3Sum-Closest/solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums==null||nums.length<3){
+            return null;
+        }
+        int depth = Integer.MAX_VALUE;
+        int result;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            int left = i+1;
+            int right = nums.length-1;
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(depth>Math.abs(sum-target)){
+                    result = sum;
+                }
+                if(sum>target){
+                    right--;
+                }else if(sum<target){
+                    left++;
+                }else{
+                    return target;
+                }
+            }
+        }
+        
+        return sum;
+    }
+}
