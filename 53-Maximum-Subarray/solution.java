b320635d062f2d751aa475fe61dc6c11e0724aed
JimmyXie123 <xiezj12345@gmail.com>
2016-06-08 01:21:31 -0400

    [53. Maximum Subarray][Time Limit Exceeded]committed by LeetCode Extension

--- 53-Maximum-Subarray/solution.java
+++ 53-Maximum-Subarray/solution.java
@@ -1,15 +1,13 @@
 public class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums==null){
+        if(nums==null||nums.length==0){
             return 0;
         }
         int[] sum = new int[nums.length];
         int max = Integer.MIN_VALUE;
-        for(int i=0; i<nums.length; i++){
-            sum[i] = 0;
-            for(int j=0; j<=i; j++){
-                sum[i] = sum[i] + nums[j];
-            }
+        sum[0] = nums[0];
+        for(int i=1; i<nums.length; i++){
+            sum[i] = sum[i-1] + nums[i];
             for(int j=-1; j<i; j++){
                 int temp;
                 if(j==-1){
