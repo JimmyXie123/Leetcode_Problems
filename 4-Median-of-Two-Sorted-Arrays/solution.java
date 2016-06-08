016ba7d68f788d56ee1a1a50ad48bf195544bd42
JimmyXie123 <xiezj12345@gmail.com>
2016-06-08 02:19:43 -0400

    [4. Median of Two Sorted Arrays][Compile Error]committed by LeetCode Extension

---
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        if(len%2==1){
+            return findKth(nums1, 0, nums2, 0, len/2+1);
+        }
+        else{
+            return (findKth(nums1, 0, nums2, 0, len/2) + findKth(nums1, 0, nums2, 0, len/2+1))/2;
+        }
+    }
+    public double findKth(int[] A, int A_start, int[] B, int B_start, int k){
+        if(A_start>=A.length){
+            return B[start+k-1];
+        }
+        if(B_start>=B.length){
+            return A[start+k-1];
+        }
+        if(k==1){
+            return Math.min(A[A_start], B[B_start]);
+        }
+        int A_key = (A_start+k/2-1>A.length-1) ? Integer.MAX_VALUE : A[A_start+k/2-1];
+        int B_key = (B_start+k/2-1>B.length-1) ? Integer.MAX_VALUE : B[B_start+k/2-1];
+        
+        if(A_key>B_key){
+            return findKth(A, A_start, B, B_start+k/2, k-k/2);
+        }else{
+            return findKth(A, A_start+k/2, B, B_start, k-k/2);
+        }
+        
+    }
+}
