afad7f2a5a28b9d04995e36ef25b56318809bff4
JimmyXie123 <xiezj12345@gmail.com>
2016-06-08 17:17:41 -0400

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -1,44 +1,80 @@
 public class Solution {
     public void sortColors(int[] nums) {
         //int[] result = new int[nums.length];
-        if(nums.size<2){
+        if(nums.length<2){
             return;
         }
         int mid = nums.length/2;
-        int[] left = new int[nums.length/2];
-        int[] right = new int[nums.length-nums.length/2];
-        for(int i=0; i<nums.length/2; i++){
+        //System.out.println(mid);
+        int[] left = new int[mid];
+        int[] right = new int[nums.length-mid];
+        for(int i=0; i<mid; i++){
             left[i] = nums[i];
         }
-        for(int i=nums.length/2; i<nums.length; i++){
-            right[i-nums.length/2] = nums[i];
+        for(int i=mid; i<nums.length; i++){
+            right[i-mid] = nums[i];
         }
+        
         sortColors(left);
+        /*
+        if(left.length == 3){
+            System.out.println();
+            for(int i:left){
+                System.out.println(i);
+            }
+        }
+        */
         sortColors(right);
-        nums = merge(left, right);
+        /*
+        if(right.length == 3){
+            System.out.println();
+            for(int i:right){
+                System.out.println(i);
+            }
+        }*/
+        merge(left, right, nums);
+        /*
+        if(nums.length == 3){
+            System.out.println();
+            for(int i:result){
+                System.out.println(i);
+            }
+        }
+        */
+        
         
     }
     
-    private int[] merge(int[] left, int[] right){
-        int[] result = new int[left.length+right.length];
+    private void merge(int[] left, int[] right, int[] result){
+        //System.out.println("到了吗");
         int i=0, j=0, l=0;
         while(i<left.length&&j<right.length){
             if(left[i]<=right[j]){
                 result[l] = left[i];
+                
                 l++; 
                 i++;
             }else{
+                
                 result[l] = right[j];
+                //System.out.println(result[0]);
                 l++;
                 j++;
             }
         }
         while(i>=left.length&&j<right.length){
-            result[l++] = right[j++];
+            
+            result[l] = right[j];
+            l++;
+            j++;
         }
         while(i<left.length&&j>=right.length){
-            result[l++] = left[i++];
+           
+            result[l] = left[i];
+             //System.out.println(result[1]);
+            l++;
+            i++;
         }
-        return result;
+        
     }
 }
