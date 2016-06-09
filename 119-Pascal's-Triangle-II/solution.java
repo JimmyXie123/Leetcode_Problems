9333b5af0eae0b15ac8b54ced942e2f5d6109143
JimmyXie123 <xiezj12345@gmail.com>
2016-06-09 11:04:40 -0400

    [119. Pascal's Triangle II][Accepted]committed by LeetCode Extension

--- 119-Pascal's-Triangle-II/solution.java
+++ 119-Pascal's-Triangle-II/solution.java
@@ -1,27 +1,19 @@
 public class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> result = new ArrayList();
-        for(int i:result){
-            System.out.println(i);
-        }
         List<Integer> temp = null;
         rowIndex++;
         int i=1;
-        while(i<=rowIndex+1){
-            result.clear();
+        while(i<=rowIndex){
+            List<Integer> result = new ArrayList();
             if(i==1){
                 result.add(1);
                 temp = result;
                 i++;
-                
-                for(int i:result){
-                    System.out.println(i);
-                }
             }else{
                 result.add(1);
                 int size = temp.size();
                 for(int j=0; j<size-1; j++){
-                    result.add(temp.get(i)+temp.get(i+1));
+                    result.add(temp.get(j)+temp.get(j+1));
                 }
                 result.add(1);
                 temp = result;
