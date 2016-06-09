7f8f35d61ef06a3e99be2090eca1f30a8c043912
JimmyXie123 <xiezj12345@gmail.com>
2016-06-09 01:41:14 -0400

    [27. Remove Element][Runtime Error]committed by LeetCode Extension

--- 27-Remove-Element/solution.java
+++ 27-Remove-Element/solution.java
@@ -1,15 +1,21 @@
 public class Solution {
-    public int removeElement(int[] A, int elem) {
-        int i = 0;
-        int pointer = A.length - 1;
-        while(i <= pointer){
-            if(A[i] == elem){
-                A[i] = A[pointer];
-                pointer--;
-            } else {
-                i++;
+    public int removeElement(int[] nums, int val) {
+        if(nums==null||nums.length==0){
+            return 0;
+        }   
+        int slow = 0;
+        int fast = 0;
+        int size = nums.length;
+        while(fast<size){
+            if(nums[fast]==val){
+                fast++;
+                size--;
+            }else{
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
             }
         }
-        return pointer + 1;
+        return size-1;
     }
 }
