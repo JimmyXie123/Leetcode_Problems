382531f26b085d29bcf45748cb3a4228efb4b870
JimmyXie123 <xiezj12345@gmail.com>
2016-06-09 01:18:38 -0400

    [11. Container With Most Water][Time Limit Exceeded]committed by LeetCode Extension

--- 11-Container-With-Most-Water/solution.java
+++ 11-Container-With-Most-Water/solution.java
@@ -7,13 +7,17 @@ public class Solution {
             return 0;
         }
         int max = 0;
-        for(int i=0; i<height.length-1; i++){
-            for(int j=i+1; j<height.length; j++){
-                if(Area(height, i, j)>max){
-                    max = Area(height, i, j);
-                }
+        int left = 0;
+        int right = height.length-1;
+        while(left<=right){
+            max = Math.max(max, Area(height, left, right));
+            if(height[left]<=height[right]){
+                left++;
+            }else{
+                right--;
             }
         }
+        
         
         return max;
     }
