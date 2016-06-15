b3b214a26ed7a861cca750540e50bd2905085800
JimmyXie123 <xiezj12345@gmail.com>
2016-06-15 13:47:31 -0400

    [85. Maximal Rectangle][Compile Error]committed by LeetCode Extension

---
+++ 85-Maximal-Rectangle/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    private int height(char[][] matrix, int i, int j){
+        int height = 0;
+        while(i<matrix.length&&matrix[i][j]!='0'){
+            height++;
+            i++;
+        }
+        return height;
+    }
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix==null){
+            return 0;
+        }
+        int max = 0;
+        for(int i=0; i<matrix.length; i++){
+            Stack<Integer> stack = new Stack();
+            for(int j=0; j<=matrix[i].length; j++){
+                int curt = (j==matrix[i].length)?-1:height(matrix, i, j);
+                while(!stack.isEmpty&&curt<=height(matrix, i, stack.peek())){
+                    int h = height(matrix, i, stack.pop().intValue());
+                    int w = (stack.isEmpty())?j:j-stack.peek()-1;
+                    max = Math.max(max, w*h);
+                }   
+            }
+        }
+        return max;
+    }
+}
