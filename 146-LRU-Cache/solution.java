31c4460b9fc5be70886f88548cb38687449af012
JimmyXie123 <xiezj12345@gmail.com>
2016-06-16 01:12:35 -0400

    [146. LRU Cache][Time Limit Exceeded]committed by LeetCode Extension

--- 146-LRU-Cache/solution.java
+++ 146-LRU-Cache/solution.java
@@ -25,7 +25,6 @@ public class LRUCache {
     
     public int get(int key) {
         if(hash.containsKey(key)){
-            System.out.println("How are u doing123");
             Node temp = head;
             Node insert = null;
             while(temp.next!=null){
@@ -47,10 +46,8 @@ public class LRUCache {
     
     public void set(int key, int value) {
         if(get(key)!=-1){
-            System.out.println("How are u doing1");
             hash.get(key).value = value;
         }else if(hash.size()==capacity){
-            System.out.println("How are u doing2");
             Node temp = head;
             while(temp.next!=null&&temp.next.next!=null){
                 temp = temp.next;
@@ -62,12 +59,10 @@ public class LRUCache {
             head.next = insert;
             hash.put(key, insert);
         }else{
-            System.out.println("How are u doing3");
             Node insert = new Node(key, value);
             insert.next = head.next;
             head.next = insert;
             hash.put(key, insert);
-            System.out.println("How are u doing3");
         }
     }
 }
