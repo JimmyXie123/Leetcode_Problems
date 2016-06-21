5bfc39bfcdd367dc5488910361b2d27657d7c02a
JimmyXie123 <xiezj12345@gmail.com>
2016-06-21 13:25:24 -0400

    [39. Combination Sum][Runtime Error]committed by LeetCode Extension

---
+++ 39-Combination-Sum/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList();
+        if(candidates==null){
+            return result;
+        }
+        List<Integer> path = new ArrayList();
+        helper(candidates, 0, target, result, path);
+        return result;
+    }
+    
+    private void helper(int[] candidates, int pos, int sum, List<List<Integer>> result, List<Integer> path){
+        if(sum==0){
+            result.add(new ArrayList(path));
+        }
+        
+        for(int i=pos; i<candidates.length; i++){
+            path.add(candidates[i]);
+            helper(candidates, i, sum-candidates[i], result, path);
+            path.remove(path.size()-1);
+        }
+        
+    }
+}
