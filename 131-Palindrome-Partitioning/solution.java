f08a0c3ba9d44f922c5d1033be467f230df087ce
JimmyXie123 <xiezj12345@gmail.com>
2016-06-21 12:10:41 -0400

    [131. Palindrome Partitioning][Accepted]committed by LeetCode Extension

--- 131-Palindrome-Partitioning/solution.java
+++ 131-Palindrome-Partitioning/solution.java
@@ -24,11 +24,7 @@ public class Solution {
     }
     
     private void helper(String s, int pos, List<String> path, List<List<String>> result){
-        int size = 0;
-        for(int i=0; i<path.size(); i++){
-                size += path.get(i).length();
-        }
-        if(size == s.length()){
+        if(pos==s.length()){
             result.add(new ArrayList(path));
         }
         
