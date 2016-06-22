14d92ffee4bb088f5569075e0ffc9532074df196
JimmyXie123 <xiezj12345@gmail.com>
2016-06-22 18:15:46 -0400

    [332. Reconstruct Itinerary][Compile Error]committed by LeetCode Extension

--- 332-Reconstruct-Itinerary/solution.java
+++ 332-Reconstruct-Itinerary/solution.java
@@ -7,20 +7,21 @@ public class Solution {
         
         HashMap<String, PriorityQueue<String>> map = new HashMap();
         for(int i=0; i<tickets.length; i++){
-            if(map.containsKey(tickets[i][0])){
-                map.get(tickets[i][0]).add(tickets[i][1]);
-            }else{
+            
+        //--------------注意这里不是if和else---------------
+            if(!map.containsKey(tickest[i][0])){
                 map.put(tickets[i][0], new PriorityQueue());
             }
+            map.get(tickets[i][0]).add(tickets[i][1]);
         }
         dfs("JFK", map, ans);
-        Collection.reverse(ans);
+        Collections.reverse(ans);
         return ans;
         
     }
     
     private void dfs(String cur, HashMap<String, PriorityQueue<String>> map, List<String> ans){
-        while(map.containsKey(cur)&&!map.get(cur).isEmpty()){
+        while(map.containsKey(cur) && !map.get(cur).isEmpty()){
             dfs(map.get(cur).poll(), map, ans);
         }
         ans.add(cur);
