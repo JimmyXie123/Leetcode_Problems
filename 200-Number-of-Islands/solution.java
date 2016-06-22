c2aadf0f96736edcaebe556db4d139397fdc0219
JimmyXie123 <xiezj12345@gmail.com>
2016-06-22 00:35:04 -0400

    [200. Number of Islands][Runtime Error]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,12 +1,13 @@
 public class Solution {
     private int[] dx = {1, 0, 0, -1};
     private int[] dy = {0, 1, -1, 0};
+    int m, n;
     private void removeIsland(char[][] grid, int x, int y){
         grid[x][y]='0';
         for(int i=0; i<4; i++){
             int nextX = x+dx[i];
             int nextY = y+dy[i];
-            if(nextX>=0&&nextX<grid[0].length&&nextY>=0&&nextY<grid.length){
+            if(nextX>=0 && nextX<n && nextY>=0&& nextY<m){
                 if(grid[nextX][nextY]=='1'){
                     removeIsland(grid, nextX, nextY);
                 }
@@ -18,11 +19,11 @@ public class Solution {
         if(grid==null||grid.length==0){
             return 0;
         }
-        int m = grid.length;
+        m = grid.length;
         if(grid[0].length==0){
             return 0;
         }
-        int n = grid[0].length;
+        n = grid[0].length;
         
         int count = 0;
         for(int i=0; i<m; i++){
