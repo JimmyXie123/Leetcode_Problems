f23339aea7a53e0c13d5c88b46cc572e62a283ed
JimmyXie123 <xiezj12345@gmail.com>
2016-06-22 16:26:55 -0400

    [301. Remove Invalid Parentheses][Compile Error]committed by LeetCode Extension

---
+++ 301-Remove-Invalid-Parentheses/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> ans = new ArrayList();
+        remove(s, ans, 0, 0, new char[2]{"(", ")"});
+        return ans;
+    }
+    
+    private void remove(String s, List<String> ans, int last_i, int last_j, char[] par){
+        for(int i=last_i, stack=0; i<s.length(); i++){
+            if(s.charAt(i)==par[0]) stack++;
+            if(s.charAt(i)==par[1]) stack--;
+            if(stack>=0) continue;
+            for(int j=last_j; j<=i; j++){
+                if((j==last_j||s.charAtCj-1)!=par[1])&&s.charAt(j)==par[1]){
+                    remove(s.substring(0, j)+s.substring(j+1, s.length), ans, i, j, par);
+                }
+            }
+        }
+        
+        String reverse = new StringBuilder(s).reverse().toString();
+        remove(reverse, ans, 0, 0, new char[2]{")", "("});
+        ans.add(reverse);
+    }
+}
