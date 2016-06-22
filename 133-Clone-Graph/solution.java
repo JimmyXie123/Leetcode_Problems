79046160c32bc5cf15bc8300574341ee512a64ef
JimmyXie123 <xiezj12345@gmail.com>
2016-06-21 23:48:22 -0400

    [133. Clone Graph][Compile Error]committed by LeetCode Extension

---
+++ 133-Clone-Graph/solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        ArrayList<UndirectedGraphNode> nodes = getNodes(node);
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> result = new HashMap();
+        for(UndirectedGraphNode n:nodes){
+            result.put(n, new UndirectedGraphNode(n.label));
+        }
+        
+        for(UndirectedGraphNode n:nodes){
+            for(UndirectedGraph w:n.neighbors){
+                UndirectedGraphNode temp = map.get(w);
+                result.get(n).neighbors.add(temp);
+            }
+        }
+        
+        return result.get(node);
+    }
+    
+    private ArrayList<UndirectedGraphNode> getNodes(UndirectedGraphNode node){
+        ArrayList<UndirectedGraphNode> nodes = new ArrayList();
+        HashSet<UndirectedGraphNode> set = new HashSet();
+        if(node==null){
+            return nodes;
+        }
+        Queue<UndirectedGraphNode> queue = new LinkedList();
+        queue.offer(node);
+        while(!queue.isEmpty()){
+            UndirectedGraphNode temp = queue.poll();
+            if(!set.contains(temp)){
+                set.add(temp);
+                nodes.add(temp);
+            }
+            for(UndirectedGraphNode n:temp.neighbors){
+                if(!set.contains(n)){
+                    queue.offer(n);
+                }
+            }
+        }
+        return nodes;
+    }
+}
