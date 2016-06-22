e66d4ba8bb1e8d54784eba795690b49b2b5afe24
JimmyXie123 <xiezj12345@gmail.com>
2016-06-21 22:20:24 -0400

    [129. Sum Root to Leaf Numbers][Runtime Error]committed by LeetCode Extension

--- 129-Sum-Root-to-Leaf-Numbers/solution.java
+++ 129-Sum-Root-to-Leaf-Numbers/solution.java
@@ -10,17 +10,21 @@
 public class Solution {
     private int result = 0;
     public int sumNumbers(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
         helper(root, 0);
         return result;
     }
     
     private void helper(TreeNode node, int sum){
-        if(node==null){
-            result += sum;
-            return null;
+        if(node.left==null&&node.right==null){
+            result += sum + node.val;
+            return;
+        }else{
+            int pass = sum*10+node.val;
+            helper(node.left, pass);
+            helper(node.right, pass);
         }
-        int pass = sum*10+node.val;
-        helper(node.left, pass);
-        helper(node.right, pass);
     }
 }
