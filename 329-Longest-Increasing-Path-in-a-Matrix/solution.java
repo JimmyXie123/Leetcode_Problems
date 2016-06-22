c3a00d1738592756a03e3e587064867dad1eec45
JimmyXie123 <xiezj12345@gmail.com>
2016-06-22 17:06:47 -0400

    [329. Longest Increasing Path in a Matrix][Compile Error]committed by LeetCode Extension

---
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    private int[] dX={-1, 0, 0, 1};
+    private int[] dY={0, -1, 1, 0};
+    int m,n;
+    public int longestIncreasingPath(int[][] matrix) {
+        if(matrix==null||matrix.length==0){
+            return 0;
+        }
+        m = matrix.length;
+        if(matrix[0].length==0){
+            return 0;
+        }
+        n = matrix[0].length;
+        
+        int max = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                max = Math.max(dfs(matrix, i, j), max);
+            }
+        }
+        
+        return max;
+    }
+    
+    private int dfs(int[][] matrix, int i, int j){
+        for(int k=0; k<4; k++){
+            int nextX = i+dX[k];
+            int nextY = j+dY[k];
+            if(nextX>=0 && nextX<m && nextY>=0 && nextY<n && matrix[i][j]<matrix[nextX][nextY]){
+                max = Math.max(max, 1+dfs(matrix, nextX, nextY));
+            }else{
+                return 0;
+            }
+        }
+        return max;
+    }
+}
