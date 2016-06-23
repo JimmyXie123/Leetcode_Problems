0cb4d159ad97a7ce690425dae321f4a387f7df85
JimmyXie123 <xiezj12345@gmail.com>
2016-06-23 15:37:57 -0400

    [51. N-Queens][Compile Error]committed by LeetCode Extension

---
+++ 51-N-Queens/solution.java
@@ -0,0 +1,56 @@
+public class Solution {
+    private List<String> draw(List<Integer> cols){
+        List<String> row = new ArrayList();
+        for(int i=0; i<cols.size(); i++){
+            String s = new String();
+            for(int j=0; j<cols.size(); j++){
+                if(j==cols.get(i)){
+                    s += "Q";
+                }else{
+                    s += ".";
+                }
+            }
+            row.add(s);
+        }
+    }
+    
+    private boolean isValid(List<Integer> cols, int col){
+        int row = cols.size();
+        for(i=0; i<row; i++){
+            if(cols.get(i)==col){
+                return false;
+            }
+            if(cols.get(i)+i==row+col){
+                return false;
+            }
+            if(cols.get(i)-i==col-row){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private void search(List<List<String>> result, List<Integer> cols, int n){
+        if(cols.size()==n){
+            result.add(draw(cols));
+        }
+        
+        for(int col=0; col<n; col++){
+            if(!isValid(cols, col)){
+                continue;
+            }
+            cols.add(col);
+            search(result, cols, n);
+            cols.remove(cols.size()-1);
+        }
+    }
+    
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList();
+        if(n<=0){
+            return result;
+        }
+        search(result, new ArrayList(), n);
+        return result;
+    }
+}
