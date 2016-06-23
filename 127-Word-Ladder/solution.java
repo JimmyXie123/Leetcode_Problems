a626acccdccbf19a335933438499a1a6f64f9a86
JimmyXie123 <xiezj12345@gmail.com>
2016-06-23 17:44:55 -0400

    [127. Word Ladder][Compile Error]committed by LeetCode Extension

---
+++ 127-Word-Ladder/solution.java
@@ -0,0 +1,58 @@
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        if(wordList==null){
+            return 0;
+        }
+        HashSet<String> graph = new HashSet();
+        Queue<String> queue = new LinkedList();
+        queue.add(beginWord);
+        graph.add(beginWord);
+        
+        int length = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            length++;
+            for(int i=0; i<size; i++){
+                String word = queue.poll();
+                for(String n:getWords(word, WordList)){
+                    if(graph.contains(n)){
+                        continue;
+                    }else{
+                        if(n.equals(endWord)){
+                            return length;
+                        }
+                    }
+                    
+                    queue.offer(n);
+                    graph.add(n);
+                }
+            }
+        }
+        
+        return length;
+        
+    }
+    
+    private String replace(String word, int i, char c){
+        char[] result = word.toCharArray();
+        result[i] = c;
+        return new String(result);
+    }
+    
+    private ArrayList<String> getWords(String word, Set<String> WordList){
+        ArrayList<String> result = new ArrayList();
+        for(int i=0; i<word.length(); i++){
+            for(char c='a'; c<='z'; c++){
+                if(word.charAt(i)==c){
+                    continue;
+                }
+                String temp = replace(word, i, c);
+                if(WordList.contains(temp)){
+                    result.add(temp);
+                }
+            }
+        }
+        
+        return result;
+    } 
+}
