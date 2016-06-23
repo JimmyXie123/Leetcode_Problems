486e37004590631e4d00f161e14ecc9cc5920154
JimmyXie123 <xiezj12345@gmail.com>
2016-06-23 13:21:04 -0400

    [310. Minimum Height Trees][Compile Error]committed by LeetCode Extension

---
+++ 310-Minimum-Height-Trees/solution.java
@@ -0,0 +1,50 @@
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        HashSet<Integer, Node> graph = new HashSet();
+        for(int i=0; i<n; i++){
+            graph.put(i, new Node(i));
+        }
+        
+        for(int[] e:edges){
+            graph.get(e[0]).neighbors.add(graph.get(e[1]));
+            graph.get(e[1]).neighbors.add(graph.get(e[0]));
+            graph.get(e[0]).degree++;
+            graph.get(e[1]).degree++;
+        }
+        
+        Queue<Node> queue = new LinkedList();
+        for(int i=0; i<n; i++){
+            if(graph.get(i).degree==1){
+                queue.offer(graph.get(i));
+            }
+        }
+        
+        while(n>2){
+            
+            Node temp = queue.poll();
+            n--;
+            for(Node side:temp.neighbors){
+                side.neighbors.remove(temp);
+                graph.remove(temp.label);
+                side.degree--;
+                if(side.degree==1){
+                    queue.offer(side);
+                }
+            }
+                
+            
+        }
+    }
+    
+}
+
+class Node{
+    int label;
+    int degree;
+    HashSet<Node> neighbors;
+    public Node(int label){
+        this.label = label;
+        degree = 0;
+        neigbors = new HashSet(); 
+    }
+}
