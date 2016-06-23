d352b40fdd983b4a72db3b43a06ca6f0f8d2d1e6
JimmyXie123 <xiezj12345@gmail.com>
2016-06-23 02:22:08 -0400

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,35 +1,41 @@
 //-------------同combination sum
 public class Solution {
-int res = Integer.MAX_VALUE;
-public int numSquares(int n) {
-    int num = 1;
-    while (num * num <= n) {
-        num++;
-    } 
-    num--;
-    ArrayList<Integer> element = new ArrayList<Integer>();
-    for (int j = num; j > 0; j--) {
-        element.add(j*j);
+    int min = Integer.MAX_VALUE;
+    public int numSquares(int n) {
+        int num = 0;
+        //int[] cache = new int[n];
+        
+        while(num*num<=n){
+            num++;
+        }
+        num--;
+        List<Integer> nums = new ArrayList();
+        for(int i=num; i>=1; i--){
+            nums.add(i*i);
+        }
+        
+        helper(nums, 0, n, 0);
+        return min;
     }
-    helper(n, element, 0, 0, 0);
-    return res;
-}
-public void helper(int n, ArrayList<Integer> element, int pos, int sum, int level) {
-    if (level > res) { //cut redundancy 
-        return;
+    
+    private void helper(List<Integer> nums, int pos, int sum, int count){
+        if(count>min){
+            return;
+        }
+        
+        if(sum<0){
+            return;
+        }
+        if(sum==0){
+            if(count<min){
+                min = count;
+            }
+        }
+        
+        for(int i= pos; i<nums.size(); i++){
+            count++;
+            helper(nums, i, sum-nums.get(i), count);
+            count--;
+        }
     }
-    if (sum > n) {
-        return;
-    }
-    if (sum == n) {
-        res = Math.min(level, res);
-        return;
-    }
-    for (int i = pos; i < element.size(); i++) {
-        sum += element.get(i);
-        helper(n, element, i, sum, level + 1);
-        sum -= element.get(i);
-
-    }
-}
 }
