32e423c09b2ced5efae05bba5131c9d56221ca34
JimmyXie123 <xiezj12345@gmail.com>
2016-06-24 16:03:14 -0400

    [80. Remove Duplicates from Sorted Array II][Compile Error]committed by LeetCode Extension

---
+++ 80-Remove-Duplicates-from-Sorted-Array-II/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums==null){
+            return 0;
+        }
+        int slow=0, fast=0;
+        int flag=1;
+        int len = nums.length;
+        while(fast<len){
+            if(nums[slow]!=nums[fast]||(nums[slow]==nums[fast]&&slow!=fast)){
+                if(nums[slow]==nums[fast]&&flag>2){
+                    coninue;
+                }else if(nums[slow]==nums[fast]&&flag<=2){
+                    flag++;
+                }else{
+                    flag = 1;
+                }
+                nums[++slow] = nums[fast++];
+            }else{
+                nums[slow] = nums[fast++];
+            }
+        }
+        
+        return slow+1;
+        
+    }
+}
