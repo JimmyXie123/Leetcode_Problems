e2f0a46480f2a001245ea8c0a0430e9566839da8
JimmyXie123 <xiezj12345@gmail.com>
2016-06-24 17:18:40 -0400

    [54. Spiral Matrix][Compile Error]committed by LeetCode Extension

---
+++ 54-Spiral-Matrix/solution.java
@@ -0,0 +1,47 @@
+public class Solution {
+    int m, n;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList();
+        if(matrix==null||matrix.length==0){
+            return result;
+        }
+        
+        row = matrix.length;
+        col = matrix[0].length;
+        
+        int count=0;
+        
+        while(count*2<row&&count*2<col){
+            for(int i=count; i<col-count; i++){
+                result.add(matrix[count][i]);
+            }
+            
+            for(int i=count+1; i<row-count; i++){
+                result.add(matrix[i][col-count-1]);
+            }
+            
+            if(row-count*2==1||col-count*2==1){
+                break;
+            }
+            
+            for(int i=col-count-2; i>=count; i--){
+                result.add(matrix[row-count-1][i]);
+            }
+            
+            for(int i=row-count-2; i>count; i--){
+                result.add(matrix[i][count]);
+            }
+            count++;
+        }
+        
+    }
+    
+    private void draw(int[][] matrix, List<Integer> result, int pos, int used, int other){
+        int start=0+k, m_or_n-1-k;
+        if(start>end){
+            return;
+        }
+        for(int i=start; i<end; )
+        
+    }
+}
