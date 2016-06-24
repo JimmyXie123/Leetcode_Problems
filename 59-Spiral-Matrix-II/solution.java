ec697baa39d030284d4ae2f95c87520d45bc2ccd
JimmyXie123 <xiezj12345@gmail.com>
2016-06-24 18:03:43 -0400

    [59. Spiral Matrix II][Compile Error]committed by LeetCode Extension

---
+++ 59-Spiral-Matrix-II/solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix;
+        if(n<=0){
+            return matrix;
+        }
+        matrix = new int[n][n];
+        int end = n*n;
+        int pointer = 1;
+        int count = 0;
+        while(count*2<n){
+            for(int i=count; i<n-count; i++){
+                matrix[count][i] = pointer++;
+            }
+            if(n-count*2==1){
+                break;
+            }
+            for(int i=count+1; i<n-count; i++){
+                matrix[i][n-count-1] = pointer++;
+            }
+            for(int i=n-count-2; i>=count; i--){
+                matrix[n-count-1][i] = pointer++;
+            }
+            for(int i=n-count-2; i>count; i--){
+                matrix[i][count] = pointer++;
+            }
+            count++;
+        }
+        
+        return matrix;
+    }
+}
