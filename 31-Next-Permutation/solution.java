7d59d6533872a723049d3758af266f0c8bd47d44
JimmyXie123 <xiezj12345@gmail.com>
2016-06-24 11:01:43 -0400

    [31. Next Permutation][Compile Error]committed by LeetCode Extension

---
+++ 31-Next-Permutation/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public void nextPermutation(int[] nums) {
+        if(nums==null){
+            return;
+        }
+        int len = nums.length;
+        for(int i=len-2; i>=0; i--){
+            if(nums[i+1]>nums[i]){
+                break;
+            }
+            int j;
+            for(int j=len-1; j>i-1; j--){
+                if(nums[j]>nums[i]){
+                    break;
+                }
+                
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                reverse(nums, i+1, len-1);
+                return;
+            }
+        }
+        
+        reverse(nums, 0, len-1);
+        
+    }
+    
+    private void reverse(int[] nums, int start, int end){
+        for(int i= start, j=end; start<end; i++, j--){
+            int temp = nums[j];
+            nums[j] = nums[i];
+            nums[i] = temp;
+        }
+    }
+}
