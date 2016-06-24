c3da7d4920d3b523eea28af99a91f4a164d9e2f6
JimmyXie123 <xiezj12345@gmail.com>
2016-06-24 11:13:08 -0400

    [31. Next Permutation][Accepted]committed by LeetCode Extension

--- 31-Next-Permutation/solution.java
+++ 31-Next-Permutation/solution.java
@@ -6,27 +6,22 @@ public class Solution {
         int len = nums.length;
         for(int i=len-2; i>=0; i--){
             if(nums[i+1]>nums[i]){
-                break;
-            }
-            for(int j=len-1; j>i-1; j--){
-                if(nums[j]>nums[i]){
-                    break;
+                for(int j=len-1; j>i-1; j--){
+                    if(nums[j]>nums[i]){
+                        int temp = nums[j];
+                        nums[j] = nums[i];
+                        nums[i] = temp;
+                        reverse(nums, i+1, len-1);
+                        return;    
+                    }
                 }
-                
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                reverse(nums, i+1, len-1);
-                return;
             }
         }
-        
         reverse(nums, 0, len-1);
-        
     }
     
     private void reverse(int[] nums, int start, int end){
-        for(int i= start, j=end; start<end; i++, j--){
+        for(int i= start, j=end; i<j; i++, j--){
             int temp = nums[j];
             nums[j] = nums[i];
             nums[i] = temp;
