5faf6eb50667dc3ae1e48f9aeff692177a102504
JimmyXie123 <xiezj12345@gmail.com>
2016-06-26 14:37:02 -0400

    [228. Summary Ranges][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 228-Summary-Ranges/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList();
+        if(nums==null){
+            return res;
+        }
+        int slow = 0;
+        int fast = 0;
+        while(fast<nums.length){
+            if(fast!=nums.length-1&&nums[fast]+1==nums[fast+1]){
+                continue;
+            }else{
+                res.add(builder(nums, slow, fast));
+                fast++;
+                slow = fast;
+            }
+        }
+        return res;
+    }
+    
+    private String builder(int[] nums, int start, int end){
+        String ele = new String();
+        if(start==end){
+            ele += nums[start];
+        }else{
+            ele = nums[start]+"->"+nums[end];
+        }
+        return ele;
+    }
+}
