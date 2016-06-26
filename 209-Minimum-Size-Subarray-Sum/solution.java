b15e44c8f7380adedce8435466561ff0bf5b461c
JimmyXie123 <xiezj12345@gmail.com>
2016-06-26 14:52:52 -0400

    [209. Minimum Size Subarray Sum][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int[] sum = new int[nums.length+1];
+        sum[0] = 0;
+        for(int i=1; i<=nums.length; i++){
+            sum[i] = sum[i-1]+nums[i-1];
+        }
+        int res = 0;
+        for(int i=1; i<=nums.length; i++){
+            for(int j=0; j<i; j++){
+                if(sum[i]-sum[j]>=s){
+                    if(res==0){
+                        res = i-j;
+                    }else{
+                        res = Math.min(i-j, res);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
