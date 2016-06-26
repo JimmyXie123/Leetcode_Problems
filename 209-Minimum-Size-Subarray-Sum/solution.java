a47d97cd795bc942dfb8711f79af1cd4721fb125
JimmyXie123 <xiezj12345@gmail.com>
2016-06-26 15:23:06 -0400

    [209. Minimum Size Subarray Sum][Accepted]committed by LeetCode Extension

--- 209-Minimum-Size-Subarray-Sum/solution.java
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -1,29 +1,28 @@
+//--------类似于subsets dfs的思想----------------
 public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
-        int[] sum = new int[nums.length+1];
-        sum[0] = 0;
-        for(int i=1; i<=nums.length; i++){
-            sum[i] = sum[i-1]+nums[i-1];
+        if(nums==null){
+            return 0;
         }
-        int res = 0;
-        int low_pos=-1;
-        for(int i=1; i<=nums.length; i++){
-            for(int j=0; j<i; j++){
-                if(low_pos!=-1&&j<=low_pos){
-                    continue;
-                }
-                if(sum[i]-sum[j]>=s){
-                    if(res==0){
-                        res = i-j;
-                    }else if(i-j>=res){
-                        continue;
-                    }else{
-                        low_pos = j;
-                        res = i-j;
-                    }
-                }
+        
+        int fast = 0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length; i++){
+            while(fast<nums.length&&sum<s){
+                sum += nums[fast++];
             }
+            if(sum>=s){
+                res = Math.min(res, fast-i);
+            }
+            sum -= nums[i];
         }
-        return res;
+        
+        if(res==Integer.MAX_VALUE){
+            return 0;
+        }else{
+            return res;
+        }
+        
     }
 }
