a5f76a0acfda99e4c986ca4c8b229b449c80496e
JimmyXie123 <xiezj12345@gmail.com>
2016-06-27 16:51:17 -0400

    [152. Maximum Product Subarray][Accepted]committed by LeetCode Extension

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -6,14 +6,15 @@ public class Solution {
         int result = nums[0];
         for(int i=1; i<nums.length; i++){
             max[i] = min[i] = nums[i];
+            //----------当负数为奇数个或者有0的时候。 动态规划：max[i], min[i]前i个数最大和最小的------------
             if(nums[i]>0){
-                max[i] = max[i-1]*nums[i];
-                min[i] = min[i-1]*nums[i];
+                max[i] = Math.max(max[i], max[i-1]*nums[i]);  
+                min[i] = Math.min(min[i], min[i-1]*nums[i]);
             }else if(nums[i]<0){
-                max[i] = min[i-1]*nums[i];
-                min[i] = max[i-1]*nums[i];
+                max[i] = Math.max(max[i], min[i-1]*nums[i]);
+                min[i] = Math.min(min[i], max[i-1]*nums[i]);
             }
-            max = Math.max(result, max[i]);
+            result = Math.max(result, max[i]);
         }
         return result;
     }
