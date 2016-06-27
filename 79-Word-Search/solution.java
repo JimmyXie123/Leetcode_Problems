47d8c54852e99a36e3d3ae7e73acd1d43117d5e8
JimmyXie123 <xiezj12345@gmail.com>
2016-06-27 16:12:53 -0400

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -8,6 +8,7 @@ public class Solution {
         }
         m = board.length;
         n = board[0].length;
+        boolean visited[][] = new boolean[m][n];
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(dfs(board, i, j, 0, word)){
@@ -19,19 +20,25 @@ public class Solution {
     }
     
     private boolean dfs(char[][] board, int i, int j, int index, String word){
-        if(index>=word.length){
+        if(index>=word.length()){
+            System.out.println("i="+i+" j="+j+" index="+index);
+            System.out.println("123");
             return true;
+        }
+        if(i<0||i>=m||j<0||j>=n){
+            return false;
         }
         if(board[i][j]!=word.charAt(index)){
             return false;
         }
+        board[i][j] = 'V';  //----------已访问过，同surrounded regions-----------
+        boolean result = false;
         for(int k=0; k<4; k++){
             int nextX = i+dX[k];
             int nextY = j+dY[k];
-            if(nextX<m&&nextX>=0&&nextY<n&&nextY>=0&&dfs(board, nextX, nextY, index+1, word)==true){
-                return true;
-            }
+            result = result||dfs(board, nextX, nextY, index+1, word);
         }
-        return false;
+        board[i][j] = word.charAt(index); //---------backtrack!-----------------
+        return result;
     }
 }
