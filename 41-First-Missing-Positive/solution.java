5a002e782f3687bc24f3343b7addb79937b66a75
JimmyXie123 <xiezj12345@gmail.com>
2016-06-27 17:55:16 -0400

    [41. First Missing Positive][Compile Error]committed by LeetCode Extension

---
+++ 41-First-Missing-Positive/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+        if(nums==null){
+            return 1;
+        }
+        for(int i=0; i<nums.length; i++){
+            while(nums[i]>0&&nums[i]<=nums.length&&nums[i]!=i+1){
+                int temp = nums[i];
+                if(nums[i]==nums[nums[i]-1]){
+                    break;
+                }
+                nums[i] = nums[nums[i]-1];
+                nums[nums[i]-1];
+            }
+            i++;
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+}
