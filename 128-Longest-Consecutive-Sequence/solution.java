69a370e50770a16fdb2327c197115d534ba9ef29
JimmyXie123 <xiezj12345@gmail.com>
2016-06-28 14:44:49 -0400

    [128. Longest Consecutive Sequence][Runtime Error]committed by LeetCode Extension

---
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums==null){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int fast = 0;
+        int len = 1;
+        int res = 1;
+        int i=0;
+        while(i<nums.length){
+            while(fast<nums.length-1&&(nums[fast]+1==nums[fast+1]||nums[fast]==nums[fast+1])){
+                fast++;
+                if(nums[fast]==nums[fast+1]){
+                    continue;
+                }
+                len++;
+            }
+            fast++;
+            i = fast;
+            res = Math.max(res, len);
+            len = 1;
+        }
+        
+        return res;
+    }
+}
