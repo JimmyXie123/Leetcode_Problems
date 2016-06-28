6b6416a2a8b9f4ce7aa214feb68054200cf5a892
JimmyXie123 <xiezj12345@gmail.com>
2016-06-28 02:41:58 -0400

    [42. Trapping Rain Water][Accepted]committed by LeetCode Extension

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -1,35 +1,27 @@
 public class Solution {
     public int trap(int[] height) {
-        if(height==null||height.length==0){
+        if(height==null){
             return 0;
         }
-        Stack<Integer> stack = new Stack();
-        
-        int result = 0;
-        for(int i=0; i<=height.length; i++){
-            if(i==0){
-                stack.push(i);
-                continue;
-            }
-            int curt = (i==height.length)?Integer.MAX_VALUE:height[i];
-            while(!stack.isEmpty()&&height[stack.peek()]<curt){
-                Integer num = stack.pop();
-                if(num.valueOf()==height.length-1&&curt==Integer.MAX_VALUE){
-                    break;
+        int left = 0;
+        int right = height.length-1;
+        int smaller = 0;
+        int res = 0;
+        while(left<right){
+            if(height[left]<height[right]){
+                smaller = height[left];
+                while(left<right&&height[left]<=smaller){
+                    res += smaller-height[left];
+                    left++;
                 }
-                int max = 0;
-                if(!stack.isEmpty()){
-                    int max_height1 = height[stack.peek()];
-                    int max_height2 = curt;
-                    int temp = Math.min(max_height1, max_height2);
-                    for(int k=stack.peek()+1; k<i; k++){
-                        max = temp-height[k];
-                    }
+            }else{
+                smaller = height[right];
+                while(left<right&&height[right]<=smaller){
+                    res += smaller-height[right];
+                    right--;
                 }
-                result = Math.max(max, result);
             }
-            stack.push(i);
         }
-        return result;
+        return res;
     }
 }
