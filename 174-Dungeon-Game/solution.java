7c1f62e1ecfb42a62cf6202b5c89aa172cdc2d9c
JimmyXie123 <xiezj12345@gmail.com>
2016-06-29 18:11:24 -0400

    [174. Dungeon Game][Accepted]committed by LeetCode Extension

--- 174-Dungeon-Game/solution.java
+++ 174-Dungeon-Game/solution.java
@@ -1,3 +1,6 @@
+/***这道题为什么自顶向下走的时候没个格子的状态是代表当前格子所需生命的最小值呢
+ * 还是代表到达那个格子的生命值和所走路径之和呢。这两个在dp传递中都要用到啊。这就是为什么不用自顶向下
+*/
 public class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
         if(dungeon==null||dungeon.length==0||dungeon[0].length==0){
@@ -6,20 +9,20 @@ public class Solution {
         int height = dungeon.length;
         int width = dungeon[0].length;
         int[][] res = new int[height][width];
-        res[0][0] = dungeon[0][0];
-        for(int i=1; i<height; i++){
-            res[i][0] = res[i-1][0]+dungeon[i][0];
+        res[height-1][width-1] = Math.max(-dungeon[height-1][width-1]+1, 1);
+        for(int i=height-2; i>=0; i--){
+            res[i][width-1] = Math.max(res[i+1][width-1]-dungeon[i][width-1], 1);
         }
-        for(int i=1; i<width; i++){
-            res[0][i] = res[0][i-1]+dungeon[i][0];
+        for(int j=width-2; j>=0; j--){
+            res[height-1][j] = Math.max(res[height-1][j+1]-dungeon[height-1][j], 1);
         }
         
-        for(int i=1; i<height; i++){
-            for(int j=1; j<width; j++){
-                res[i][j] = Math.min(res[i-1][j], res[i][j-1])+dungeon[i][j];
+        for(int i=height-2; i>=0; i--){
+            for(int j=width-2; j>=0; j--){
+                int temp = Math.min(res[i+1][j], res[i][j+1]);
+                res[i][j] = Math.max(temp-dungeon[i][j], 1); 
             }
         }
-        
-        return (res[height-1][width-1]<0)?-res[height-1][width-1]:0;
+        return res[0][0];
     }
 }
