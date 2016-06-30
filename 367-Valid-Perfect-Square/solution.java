2c30931ae042f7daa3a5fdc6961c21304e3c0e2c
JimmyXie123 <xiezj12345@gmail.com>
2016-06-30 02:43:26 -0400

    [367. Valid Perfect Square][Accepted]committed by LeetCode Extension

---
+++ 367-Valid-Perfect-Square/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start = 0, end = num;
+        while(start+1<end){
+            long mid = start+(end-start)/2;
+            if(mid*mid==num){
+                return true;
+            }else if(mid*mid<num){
+                start = mid;
+            }else{
+                end = mid;
+            }
+        }
+        
+        if(start*start==num){
+            return true;
+        }else if(end*end==num){
+            return true;
+        }
+        return false;
+    }
+}
