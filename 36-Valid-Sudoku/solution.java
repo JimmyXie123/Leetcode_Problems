9ea95a62c0979ae9ba1673df4421684f96ffd06e
JimmyXie123 <xiezj12345@gmail.com>
2016-07-01 19:15:41 -0400

    [36. Valid Sudoku][Compile Error]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -26,13 +26,13 @@ public class Solution {
         
         System.out.println("123");
         //subgrid
-        for(int i=1; i<9; i=i+3){
+        for(int i=0; i<9; i=i+3){
             Arrays.fill(visited, false);
-            for(int j=1; j<9; j=j+3){
-                for(int w=0; w<9; w++){
+            for(int j=0; j<9; j=j+3){
+                
                     if(!process(visited, board, i + w/3, j + w%3))
                         return false;        
-                    }
+                    
             }
         }
         
