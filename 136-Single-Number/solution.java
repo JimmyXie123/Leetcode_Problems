6df425a04f64f9fbc91745994ab845fe43e66d02
JimmyXie123 <xiezj12345@gmail.com>
2016-07-01 18:03:24 -0400

    [136. Single Number][Compile Error]committed by LeetCode Extension

---
+++ 136-Single-Number/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet();
+        for(int i:nums){
+            if(set.contains(i)){
+                set.remove(i);
+            }else{
+                set.add(i);
+            }
+        }
+        int res;
+        for(int i:set){
+            res = i;
+        }
+        return res;
+    }
+}
