464f251d0960ab1ab0558068e94871b25df59f0f
JimmyXie123 <xiezj12345@gmail.com>
2016-07-02 17:00:56 -0400

    [37. Sudoku Solver][Compile Error]committed by LeetCode Extension

--- 37-Sudoku-Solver/solution.java
+++ 37-Sudoku-Solver/solution.java
@@ -8,12 +8,12 @@ public class Solution {
     private boolean solver(char[][] board){
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(board[i][j]=='.'){
+                if(board[i][j]!='.'){
                     continue;
                 }
                 for(int k=1; k<=9; k++){
                     board[i][j] = (char)(k+'0');
-                    if(solver(board)&&isValid(board, i, j)){
+                    if(isValid(board, i, j)&&solver(board)){
                         return true;
                     }
                     board[i][j] = '.';  //-------------think about backtracking here.----------------
@@ -29,29 +29,30 @@ public class Solution {
         for(int j=0; j<9; j++){
             if(set.contains(board[a][j])){
                 return false;
-            }else{
-                set.add(board[a][j]);
             }
+             if(board[a][j]>'0' && board[a][j]<='9')
+                contained.add(board[a][j]);
         }
         set.clear();
         for(int i=0; i<9; i++){
             if(set.contains(board[i][b])){
                 return false;
-            }else{
-                set.add(board[i][b]);
             }
+            if(board[i][b]>'0' && board[i][b]<='9')
+                contained.add(board[i][b]);
         }
+        
         set.clear();
-        int startX = a/3*3;
-        int startY = b/3*3;
+        int startX = a/3*3+1;
+        int startY = b/3*3+1;
         for(int k=0; k<9; k++){
             int nextX = startX+dX[k];
             int nextY = startY+dY[k];
             if(set.contains(board[nextX][nextY])){
                 return false;
-            }else{
-                set.add(board[nextX][nextY]);
             }
+             if(board[nextX][nextY]>'0' && board[nextX][nextY]<='9')
+                contained.add(board[nextX][nextY]);
         }
         return true;
     }
