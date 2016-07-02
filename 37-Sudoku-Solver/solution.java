8fc69938da596c7b8832c5dff670bf3a942bc8a3
JimmyXie123 <xiezj12345@gmail.com>
2016-07-02 16:42:14 -0400

    [37. Sudoku Solver][Runtime Error]committed by LeetCode Extension

---
+++ 37-Sudoku-Solver/solution.java
@@ -0,0 +1,58 @@
+public class Solution {
+    int[] dX = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+    int[] dY = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+    public void solveSudoku(char[][] board) {
+        solver(board); 
+    }
+    
+    private boolean solver(char[][] board){
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                if(board[i][j]=='.'){
+                    continue;
+                }
+                for(int k=1; k<=9; k++){
+                    board[i][j] = (char)(k+'0');
+                    if(solver(board)&&isValid(board, i, j)){
+                        return true;
+                    }
+                    board[i][j] = '.';  //-------------think about backtracking here.----------------
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int a, int b){
+        HashSet<Character> set = new HashSet(); 
+        for(int j=0; j<9; j++){
+            if(set.contains(board[a][j])){
+                return false;
+            }else{
+                set.add(board[a][j]);
+            }
+        }
+        set.clear();
+        for(int i=0; i<9; i++){
+            if(set.contains(board[i][b])){
+                return false;
+            }else{
+                set.add(board[i][b]);
+            }
+        }
+        set.clear();
+        int startX = a/3*3;
+        int startY = b/3*3;
+        for(int k=0; k<9; k++){
+            int nextX = startX+dX[k];
+            int nextY = startY+dY[k];
+            if(set.contains(board[nextX][nextY])){
+                return false;
+            }else{
+                set.add(board[nextX][nextY]);
+            }
+        }
+        return true;
+    }
+}
