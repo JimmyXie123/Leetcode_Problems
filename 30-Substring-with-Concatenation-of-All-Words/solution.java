dc2d99e453539a4b20b4bc1df7bf473134113432
JimmyXie123 <xiezj12345@gmail.com>
2016-07-03 15:27:40 -0400

    [30. Substring with Concatenation of All Words][Compile Error]committed by LeetCode Extension

---
+++ 30-Substring-with-Concatenation-of-All-Words/solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList();
+        int m = words.length;
+        int n = words[0].length;
+        HashMap<String, Integer> toFind = new HashMap();
+        HashMap<String, Integer> Found = new HashMap;
+        
+        for(String word:words){
+            if(toFind.containsKey(word)){
+                toFind.put(word, toFind.get(word)+1);
+            }else{
+                toFind.put(word, 1);
+            }
+        }
+        
+        for(int i=0; i<=s.length-m*n; i++){
+            Found.clear();
+            int j=0;
+            for(j=0; j<m; j++){
+                int k = i+j*n;
+                String temp = s.substring(k, k+m);
+                if(!toFind.containsKey(temp)){
+                    break;
+                }
+                if(Found.containsKey(temp)){
+                    Found.put(temp, Found.get(temp)+1);
+                }else{
+                    Found.put(temp, 1);
+                }
+                if(Found.get(temp)>toFind.get(temp)){
+                    break;
+                }
+            }
+            if(j==m) res.add(i);
+        }
+        
+        return res;
+        
+    }
+}
