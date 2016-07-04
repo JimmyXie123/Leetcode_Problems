da060696a956bb9fcc88073fcf3a940792457ae0
JimmyXie123 <xiezj12345@gmail.com>
2016-07-04 04:13:19 -0400

    [299. Bulls and Cows][Accepted]committed by LeetCode Extension

---
+++ 299-Bulls-and-Cows/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public String getHint(String secret, String guess) {
+        int numA = 0;
+        int numB = 0;
+        String res = "";
+        HashMap<Character, Integer> map = new HashMap();
+        for(int i=0; i<secret.length(); i++){
+            if(secret.charAt()==)
+        }
+        
+    }
+}
