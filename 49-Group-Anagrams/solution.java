0777befad0af1e6388ed77fbde27f8f39ddbbb7b
JimmyXie123 <xiezj12345@gmail.com>
2016-07-04 17:06:07 -0400

    [49. Group Anagrams][Compile Error]committed by LeetCode Extension

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -1,44 +1,15 @@
 public class Solution {
-    private boolean isAnagram(String source, String target){
-        if(source.length()!=target.length()){
-            return false;
-        }
-        int[] hash = new int[256];
-        for(char ch:source.toCharArray()){
-            hash[ch]++;
-        }
-        for(char ch:target.toCharArray()){
-            hash[ch]--;
-            if(hash[ch]<0){
-                return false;
-            }
-        }
-        return true;
-    }
-    
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList();
-        List<String> path = new ArrayList();
-        if(strs==null){
-            return res;
-        }
-        int len = strs.length;
-        boolean[] visited= new boolean[len];
-        for(int i=0; i<len; i++){
-            if(visited[i]==true){
-                continue;
+        HashMap<char[], ArrayList<String>> map = new HashMap();
+        for(String str:strs){
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            if(!map.contains(ch)){
+                map.put(ch, new ArrayList());
+            }else{
+                map.get(ch).add(str);
             }
-            path.clear();
-            path.add(strs[i]);
-            visited[i] = true;
-            for(int j=i+1; j<len; j++){
-                if(isAnagram(strs[i], strs[j])){
-                    path.add(strs[j]);
-                    visited[j] = true;
-                }
-            }
-            res.add(new ArrayList(path));
         }
-        return res;
+        return new ArrayList(map.values());
     }
 }
