1e4af46ba43f5cddfaf21954b8c001346b870eab
JimmyXie123 <xiezj12345@gmail.com>
2016-07-04 03:08:05 -0400

    [242. Valid Anagram][Runtime Error]committed by LeetCode Extension

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -1,5 +1,8 @@
 public class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
         HashMap<Character, Integer> toFind = new HashMap();
         HashMap<Character, Integer> Found = new HashMap();
         for(char ch:s.toCharArray()){
@@ -12,14 +15,20 @@ public class Solution {
         
         for(char ch:t.toCharArray()){
             if(Found.containsKey(ch)){
-                Found.put(ch, toFind.get(ch)+1);
+                Found.put(ch, Found.get(ch)+1);
             }else{
                 Found.put(ch, 1);
             }
         }
         
         for(char ch:toFind.keySet()){
-            if(!Found.containsKey(ch)||Found.get(ch)!=toFind.get(ch)){
+            int a1 = Found.get(ch);
+            int a2 = toFind.get(ch);
+            if((!Found.containsKey(ch))||a1!=a2){
+                /*System.out.println(ch);
+                System.out.println(Found.get(ch)+" "+toFind.get(ch));
+                System.out.println(Found.get(ch)!=toFind.get(ch));
+                System.out.println(!Found.containsKey(ch));*/
                 return false;
             }
         }
