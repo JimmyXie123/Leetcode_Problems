f2c2e3bdd39d46ccd45f511c57ee6b6b955cf935
JimmyXie123 <xiezj12345@gmail.com>
2016-07-09 15:28:27 -0400

    [246. Strobogrammatic Number][Accepted]committed by LeetCode Extension

--- 246-Strobogrammatic-Number/solution.java
+++ 246-Strobogrammatic-Number/solution.java
@@ -1,30 +1,19 @@
 public class Solution {
     public boolean isStrobogrammatic(String num) {
-        if(num==null){
+        if (num == null) {
             return false;
         }
-        String dic = "69180";
-        int len = num.length();
-        //HashMap<Integer, Character> map = new HashMap();
-        for(int i=0; i<len; i++){
-            if(!dic.contains(""+num.charAt(i))){
+        Map<Character, Character> map = new HashMap<>();
+        map.put('0','0'); map.put('1', '1'); map.put('6','9'); 
+        map.put('8','8'); map.put('9','6');
+        int l = 0, r = num.length()-1;
+        while (l <= r) {
+            char ll = num.charAt(l), rr = num.charAt(r);
+            if (! map.containsKey(ll) || rr != map.get(ll)) {
                 return false;
             }
-            if(num.charAt(i)=='1'&&num.charAt(len-i-1)=='1'){
-                continue;
-            }else if(num.charAt(i)=='0'&&num.charAt(len-i-1)=='0'){
-                System.out.println("123");
-                continue;
-            }else if(num.charAt(i)=='6'&&num.charAt(len-i-1)=='9'){
-                continue;
-            }else if(num.charAt(i)=='9'&&num.charAt(len-i-1)=='6'){
-                continue;
-            }else if(num.charAt(i)=='8'&&num.charAt(len-i-1)=='8'){
-                continue;
-            }
-            return false;
-            
-            
+            l++;
+            r--;
         }
         return true;
     }
