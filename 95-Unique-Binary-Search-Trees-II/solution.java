63eb9f636417c586b91eb826c30e68392b9fe14c
JimmyXie123 <xiezj12345@gmail.com>
2016-07-10 18:11:01 -0400

    [95. Unique Binary Search Trees II][Accepted]committed by LeetCode Extension

--- 95-Unique-Binary-Search-Trees-II/solution.java
+++ 95-Unique-Binary-Search-Trees-II/solution.java
@@ -16,27 +16,25 @@ public class Solution {
     }
     
     private List<TreeNode> generate(int start, int end){
-        List<TreeNode> rst = new ArrayList<TreeNode>();   
-    
-        if(start > end){
-            rst.add(null);
-            return rst;
+        List<TreeNode> res = new ArrayList();
+        
+        if(start>end){
+            res.add(null);
+            return res;
         }
-     
-            for(int i=start; i<=end; i++){
-                List<TreeNode> left = generate(start, i-1);
-                List<TreeNode> right = generate(i+1, end);
-                for(TreeNode l: left){
-                    for(TreeNode r: right){
-// should new a root here because it need to 
-// be different for each tree
-                        TreeNode root = new TreeNode(i);  
-                        root.left = l;
-                        root.right = r;
-                        rst.add(root);
-                    }
+        
+        for(int i=start; i<=end; i++){
+            List<TreeNode> left = generate(start, i-1);
+            List<TreeNode> right = generate(i+1, end);
+            for(TreeNode l:left){
+                for(TreeNode r:right){
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
                 }
             }
-        return rst;
+        }
+        return res;
     }
 }
