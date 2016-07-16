92f56ea801f993fcd9acdc5785cab41810b699c2
JimmyXie123 <xiezj12345@gmail.com>
2016-07-16 00:00:42 -0400

    [22. Generate Parentheses][Accepted]committed by LeetCode Extension

---
+++ 22-Generate-Parentheses/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList();
+        String path = "";
+        helper(res, path, n, n);
+        return res;
+    }
+    
+    private void helper(List<String> res, String path, int left, int right){
+        if(left==0&&right==0){
+            res.add(new String(path));
+        }
+        
+        if(left>0){
+            helper(res, path+"(", left-1, right);
+        }
+        
+        if(right>0&&right>left){
+            helper(res, path+")", left, right-1);
+        }
+    }
+}
