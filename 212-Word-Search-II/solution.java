3da60c89cd151bede7093a03e11d548d7c0706a9
JimmyXie123 <xiezj12345@gmail.com>
2016-07-29 14:24:45 -0400

    [212. Word Search II][Accepted]committed by LeetCode Extension

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -33,7 +33,6 @@ public class Solution {
         for(String word: words){
             AddWord(word);
         }
-        //System.out.println(root.kids[4].kids[0].kids['t'-'a'].hasWord);
         
         HashSet<String> set = new HashSet();
         String path = "";
@@ -41,7 +40,7 @@ public class Solution {
         
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                helper(board, i, j, root, res, path, set, visited);
+                helper(board, i, j, root, res, path, set);
             }
         }
         
@@ -49,23 +48,23 @@ public class Solution {
         
     }
     
-    private void helper(char[][] board, int i, int j, Node now, List<String> res, String path, HashSet<String> set, boolean[][] visited){
+    private void helper(char[][] board, int i, int j, Node now, List<String> res, String path, HashSet<String> set){
         if(now!=null&&now.hasWord&&!set.contains(path)){
             res.add(path);
             set.add(path);
         }
-        if(i<0||i>=m||j<0||j>=n||visited[i][j]==true||now==null){
+        if(i<0||i>=m||j<0||j>=n||board[i][j]==0||now==null){
             return;
         }
         
         path = path+board[i][j];
-        
-        visited[i][j] = true;
+        char tmp = board[i][j];
+        board[i][j] = 0;
         for(int k=0; k<4; k++){
             int nextX = i+dX[k];
             int nextY = j+dY[k];
-            helper(board, nextX, nextY, now.kids[board[i][j]-'a'], res, path, set, visited);
+            helper(board, nextX, nextY, now.kids[tmp-'a'], res, path, set);
         }
-        visited[i][j] = false;
+        board[i][j] = tmp;
     }
 }
