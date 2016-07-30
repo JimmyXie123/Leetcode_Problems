3e470ad98174cdd3b1741a8bf0e751b6cb215eb6
JimmyXie123 <xiezj12345@gmail.com>
2016-07-30 16:07:05 -0400

    [248. Strobogrammatic Number III][Accepted]committed by LeetCode Extension

---
+++ 248-Strobogrammatic-Number-III/solution.java
@@ -0,0 +1,37 @@
+public class Solution {
+    int res = 0;
+    public int strobogrammaticInRange(String low, String high) {
+        HashMap<Character, Character> map = new HashMap();
+        map.put('1', '1');
+        map.put('6', '9');
+        map.put('9', '6');
+        map.put('8', '8');
+        map.put('0', '0');
+        for(int i=low.length(); i<=high.length(); i++){
+            dfs(low, high, new char[i], 0, i-1, map);
+        }
+        return res;
+    }
+    
+    private void dfs(String low, String high, char c[], int left, int right, HashMap<Character, Character> map){
+        if(left>right){
+            String s = new String(c);
+            if((s.length()==low.length()&&s.compareTo(low)<0)||(s.length()==high.length()&&s.compareTo(high)>0)){
+                return;
+            }
+            res++;
+            return;
+        }
+        
+        for(Character ch:map.keySet()){
+            //if(c.length!=1&&ch=='0') {
+            //    continue;
+            //}
+            //if(c.length%2==1&&left==right&&(ch=='6'||ch=='9')) continue;
+            c[left] = ch;
+            c[right] = map.get(ch);
+            if(c.length != 1 && c[0] == '0') continue;
+		   if(left < right || left == right && ch==map.get(ch)) dfs(low, high, c, left + 1, right - 1, map);
+        }
+    }
+}
