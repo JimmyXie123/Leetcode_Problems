b5cdc235cb1cf1cdcae2f915383ca540e241976a
JimmyXie123 <xiezj12345@gmail.com>
2016-08-03 04:03:32 -0400

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,33 +1,30 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-
-    if (s == null || p == null) {
-        return false;
-    }
-    boolean[][] dp = new boolean[s.length()+1][p.length()+1];
-    dp[0][0] = true;
-    for (int i = 0; i < p.length(); i++) {
-        if (p.charAt(i) == '*' && dp[0][i-1]) {
-            dp[0][i+1] = true;
+        int m = s.length();
+        int n = p.length();
+        boolean dp[][] = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for(int i=2; i<=n; i++){
+            if(p.charAt(i-1)=='*'&&dp[0][i-2]){
+                dp[0][i] = true;
+            }
         }
-    }
-    for (int i = 0 ; i < s.length(); i++) {
-        for (int j = 0; j < p.length(); j++) {
-            if (p.charAt(j) == '.') {
-                dp[i+1][j+1] = dp[i][j];
-            }
-            if (p.charAt(j) == s.charAt(i)) {
-                dp[i+1][j+1] = dp[i][j];
-            }
-            if (p.charAt(j) == '*') {
-                if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
-                    dp[i+1][j+1] = dp[i+1][j-1];
-                } else {
-                    dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+        
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(p.charAt(j-1)==s.charAt(i-1)||p.charAt(j-1)=='.'){
+                    dp[i][j]=dp[i-1][j-1];
+                }else if(j>=2&&p.charAt(j-1)=='*'){
+                    if(s.charAt(i-1)!=p.charAt(j-2)&&p.charAt(j-2)!='.'){
+                        dp[i][j] = dp[i][j-2];
+                    }else{
+                        dp[i][j] = dp[i][j-2] || dp[i-1][j] || dp[i][j-1];
+                    }
                 }
             }
         }
+        
+        return dp[m][n];
+        
     }
-    return dp[s.length()][p.length()];
-}
 }
