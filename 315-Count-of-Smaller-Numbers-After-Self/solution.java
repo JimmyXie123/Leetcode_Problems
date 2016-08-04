db6231af7e96d74c7fc4256784b0766f2b0a4f03
JimmyXie123 <xiezj12345@gmail.com>
2016-08-04 12:21:05 -0400

    [315. Count of Smaller Numbers After Self][Accepted]committed by LeetCode Extension

---
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> sorted = new ArrayList();
+        Integer[] res = new Integer[nums.length];
+        for(int i=nums.length-1; i>=0; i--){
+            int index = findIndex(sorted, nums[i]);
+            sorted.add(index, nums[i]);
+            res[i] = new Integer(index);
+        }
+        return Arrays.asList(res);
+    }
+    
+    private int findIndex(List<Integer> sorted, int num){     
+        //--------strictly less than, so no "==" case in binary search--------------
+        if(sorted.size()==0)    return 0;         
+        int start = 0;
+        int end = sorted.size()-1;
+        if(num<sorted.get(start))  return 0;
+        if(num>sorted.get(end))  return end+1;
+        while(start+1<end){
+            int mid = start+(end-start)/2;
+            if(sorted.get(mid)<num){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        if(sorted.get(start)>=num){
+            return start;
+        }
+        return end;
+    }
+}
