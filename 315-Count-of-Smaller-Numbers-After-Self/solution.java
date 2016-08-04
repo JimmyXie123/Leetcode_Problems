77b4e1496f93f0395c6e26645b7d58f31d2b385d
JimmyXie123 <xiezj12345@gmail.com>
2016-08-04 12:34:21 -0400

    [315. Count of Smaller Numbers After Self][Accepted]committed by LeetCode Extension

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -20,7 +20,7 @@ public class Solution {
         while(start+1<end){
             int mid = start+(end-start)/2;
             if(sorted.get(mid)<num){
-                start = mid;         //----------------------
+                start = mid;         
             }else{
                 end = mid;
             }
