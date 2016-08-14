0483f03445627e23e1bad004ad2fb3ee1c6ebad2
JimmyXie123 <xiezj12345@gmail.com>
2016-08-14 14:51:01 -0400

    [381. Insert Delete GetRandom O(1) - Duplicates allowed][Accepted]committed by LeetCode Extension

--- 381-Insert-Delete-GetRandom-O(1)---Duplicates-allowed/solution.java
+++ 381-Insert-Delete-GetRandom-O(1)---Duplicates-allowed/solution.java
@@ -1,8 +1,8 @@
 import java.util.Random;
 
 public class RandomizedCollection {
-    ArrayList<Integer> nums;
-    HashMap<Integer, HashSet<Integer>> map;
+    List<Integer> nums;
+    Map<Integer, Set<Integer>> map;
     Random random = new Random();
     /** Initialize your data structure here. */
     public RandomizedCollection() {
@@ -12,9 +12,8 @@ public class RandomizedCollection {
     
     /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
     public boolean insert(int val) {
-        boolean res = false;
-        if(!map.containsKey(val)){
-            res = true;
+        boolean res = map.containsKey(val);
+        if(!res){
             map.put(val, new HashSet());
             map.get(val).add(nums.size());
             nums.add(val);
@@ -27,21 +26,22 @@ public class RandomizedCollection {
     
     /** Removes a value from the collection. Returns true if the collection contained the specified element. */
     public boolean remove(int val) {
-        boolean res = false;
-        if(!map.containsKey(val)){
-            return false;
+        boolean res = map.containsKey(val);
+        if(!res){
+            return res;
         }else{
             int index = map.get(val).iterator().next();
             map.get(val).remove(index);
             if(index<nums.size()-1){
-                map.get(nums.get(nums.size()-1)).add(index);
-                map.get(nums.get(nums.size()-1)).remove(nums.size()-1);
+                int lastone = nums.get(nums.size()-1);
+                nums.set(index, lastone);
+                map.get(lastone).add(index);
+                map.get(lastone).remove(nums.size()-1);
             }
-            nums.set(index, nums.get(nums.size()-1));
-            nums.remove(nums.size()-1);
             if(map.get(val).size()==0)  map.remove(val);
-            return true;
+            nums.remove(nums.size()-1);
         }
+        return res;
     }
     
     /** Get a random element from the collection. */
