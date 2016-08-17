fbea07b01541841ca884bb37dff7ef1a310d64af
JimmyXie123 <xiezj12345@gmail.com>
2016-08-16 22:40:06 -0400

    [95. Unique Binary Search Trees II][Accepted]committed by LeetCode Extension

--- 95-Unique-Binary-Search-Trees-II/solution.java
+++ 95-Unique-Binary-Search-Trees-II/solution.java
@@ -9,22 +9,22 @@
  */
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
-        return helper(1, n);
+        return helper(n, 0);
     }
     
-    private List<TreeNode> helper(int start, int end){
+    private List<TreeNode> helper(int n, int makeup){
         List<TreeNode> res = new ArrayList();
-        if(start>end){
+        if(n==0){
             return res;
         }
         
-        for(int i=start; i<=end; i++){
-            List<TreeNode> left = helper(start, i-1);
-            List<TreeNode> right = helper(i+1, end);
+        for(int i=1; i<=n; i++){
+            List<TreeNode> left = helper(i-1, makeup);
+            List<TreeNode> right = helper(n-i, makeup+i);
             if(!left.isEmpty()&&!right.isEmpty()){
                 for(TreeNode l:left){
                     for(TreeNode r:right){
-                        TreeNode root = new TreeNode(i);
+                        TreeNode root = new TreeNode(i+makeup);
                         root.left = l;
                         root.right = r;
                         res.add(root);
@@ -32,18 +32,18 @@ public class Solution {
                 }
             }else if(!left.isEmpty()&&right.isEmpty()){
                 for(TreeNode l:left){
-                    TreeNode root = new TreeNode(i);
+                    TreeNode root = new TreeNode(i+makeup);
                     root.left = l;
                     res.add(root);
                 }
             }else if(left.isEmpty()&&!right.isEmpty()){
                 for(TreeNode r:right){
-                    TreeNode root = new TreeNode(i);
+                    TreeNode root = new TreeNode(i+makeup);
                     root.right = r;
                     res.add(root);
                 }
             }else{
-                TreeNode root = new TreeNode(i);
+                TreeNode root = new TreeNode(i+makeup);
                 res.add(root);
             }
         }
