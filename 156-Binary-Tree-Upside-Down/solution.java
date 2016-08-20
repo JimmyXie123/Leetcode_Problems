d1b91e270e53dee86ef97a8475bdc191ab6791c5
JimmyXie123 <xiezj12345@gmail.com>
2016-08-19 22:28:55 -0400

    [156. Binary Tree Upside Down][Accepted]committed by LeetCode Extension

--- 156-Binary-Tree-Upside-Down/solution.java
+++ 156-Binary-Tree-Upside-Down/solution.java
@@ -9,9 +9,10 @@
  */
 public class Solution {
     public TreeNode upsideDownBinaryTree(TreeNode root) {
-        if(root==null||root.left==null&&root.right==null){
+        if(root==null||(root.left==null&&root.right==null)){
             return root;
         }
+        
         TreeNode newRoot = upsideDownBinaryTree(root.left);
         root.left.left = root.right;
         root.left.right = root;
