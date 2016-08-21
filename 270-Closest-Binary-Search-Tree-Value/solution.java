9f756654f30df5ceb4db99fb23d1eb32be7d7aec
JimmyXie123 <xiezj12345@gmail.com>
2016-08-20 23:50:28 -0400

    [270. Closest Binary Search Tree Value][Accepted]committed by LeetCode Extension

--- 270-Closest-Binary-Search-Tree-Value/solution.java
+++ 270-Closest-Binary-Search-Tree-Value/solution.java
@@ -9,20 +9,13 @@
  */
 public class Solution {
     public int closestValue(TreeNode root, double target) {
-        Integer res = null;
-        while(root!=null){
-            if(res==null)   res = root.val;
-            if(Math.abs(root.val-target)<Math.abs(res-target)){
-                res = root.val;
-            }
-            if(root.val>target) {
-                root = root.left;
-            }else if(root.val<target){
-                root = root.right;
-            }else{
-                return root.val;
-            }
-        }
-        return res;
+        int ret = root.val;   
+    while(root != null){
+        if(Math.abs(target - root.val) < Math.abs(target - ret)){
+            ret = root.val;
+        }      
+        root = root.val > target? root.left: root.right;
+    }     
+    return ret;
     }
 }
