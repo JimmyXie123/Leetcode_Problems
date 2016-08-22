3ecbffc600d32ab9f3490678344b4ed146a92394
JimmyXie123 <xiezj12345@gmail.com>
2016-08-22 01:58:39 -0400

    [285. Inorder Successor in BST][Accepted]committed by LeetCode Extension

--- 285-Inorder-Successor-in-BST/solution.java
+++ 285-Inorder-Successor-in-BST/solution.java
@@ -9,27 +9,24 @@
  */
 public class Solution {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        TreeNode successor = null;
-        while(root!=null&&root.val!=p.val){
-            if(root.val>p.val){
-                successor = root;
-                root = root.left;
-            }else{
+        if(root==null||p==null) return null;
+        TreeNode post = null;
+        while(root!=null){
+            if(root.val<p.val){
                 root = root.right;
+            }else if(root.val>p.val){
+                post = root;
+                root= root.left;
+            }else{
+                if(root.right!=null){
+                    post = root.right;
+                    while(post.left!=null){
+                        post = post.left;
+                    }
+                }
+                break;
             }
         }
-        
-        if(root==null){
-            return null;
-        }
-        
-        if(root.right==null){
-            return successor;
-        }
-        root = root.right;
-        while(root.left!=null){
-            root = root.left;
-        }
-        return root;
+        return post;
     }
 }
