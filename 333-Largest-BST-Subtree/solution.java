9312b55de78f153109552ad9a6e5211d2ab50626
JimmyXie123 <xiezj12345@gmail.com>
2016-08-22 14:32:30 -0400

    [333. Largest BST Subtree][Accepted]committed by LeetCode Extension

--- 333-Largest-BST-Subtree/solution.java
+++ 333-Largest-BST-Subtree/solution.java
@@ -7,60 +7,51 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- 
 class returnType{
-    boolean BST;
-    int num;
-    int min; 
+    boolean isBST;
     int max;
-    public returnType(boolean BST, int num, int min, int max){
-        this.BST = BST;
-        this.num = num;
-        this.min = min;
+    int min;
+    int num;
+    public returnType(boolean isBST, int max, int min, int num){
+        this.isBST = isBST;
         this.max = max;
-    }
-}  
-
-public class Solution {
-    int count=0;
-    public int largestBSTSubtree(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
-        returnType tmp = helper(root);
-        return count;
-    }
-    
-    private returnType helper(TreeNode node){
-        if(node.left==null&&node.right==null){
-            count = Math.max(count, 1);
-            return new returnType(true, 1, node.val, node.val);
-        }else if(node.left!=null&&node.right==null){
-            returnType left = helper(node.left);
-            if(left.max<node.val&&left.BST){
-                count = Math.max(left.num+1, count);
-                return new returnType(true, left.num+1, left.min, node.val); 
-            }else{
-                return new returnType(false, left.num, left.min, node.val);
-            }
-        }else if(node.left==null&&node.right!=null){
-            returnType right = helper(node.right);
-            if(right.min>node.val&&right.BST){
-                count = Math.max(right.num+1, count);
-                return new returnType(true, right.num+1, node.val, right.max); 
-            }else{
-                return new returnType(false, right.num, node.val, right.max);
-            }
-        }else{
-            returnType left = helper(node.left);
-            returnType right = helper(node.right);
-            if(left.max<node.val&&left.BST&&right.BST&&right.min>node.val){
-                count = Math.max(left.num+right.num+1, count);
-                return new returnType(true, left.num+right.num+1, left.min, right.max); 
-            }else{
-                return new returnType(false, left.num+right.num, left.min, right.max);
-            }
-        }
+        this.min = min;
+        this.num = num;
     }
 }
 
+
+public class Solution {
+    public int largestBSTSubtree(TreeNode root) {
+        if(root==null)  return 0;
+        return helper(root).num;
+    }
+    
+    private returnType helper(TreeNode node){
+       if(node.left!=null&&node.right!=null){
+           returnType left = helper(node.left);
+           returnType right = helper(node.right);
+           if(left.isBST&&right.isBST&&node.val>left.max&&node.val<right.min){
+               return new returnType(true, right.max, left.min, left.num+right.num+1);
+           }else{
+               return new returnType(false, right.max, left.min, Math.max(left.num, right.num));
+           }
+       }else if(node.left!=null){
+           returnType left = helper(node.left);
+           if(left.isBST&&node.val>left.max){
+               return new returnType(true, node.val, left.min, left.num+1);
+           }else{
+               return new returnType(false, node.val, left.min, left.num);
+           }
+       }else if(node.right!=null){
+           returnType right = helper(node.right);
+           if(right.isBST&&node.val<right.min){
+               return new returnType(true, right.max, node.val, right.num+1);
+           }else{
+               return new returnType(false, right.max, node.val, right.num);
+           }
+       }else{
+           return new returnType(true, node.val, node.val, 1);
+       }
+    }
+}
