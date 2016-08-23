6b47636b16ef5b83f826e4d66fb146bea0af0b06
JimmyXie123 <xiezj12345@gmail.com>
2016-08-22 21:52:48 -0400

    [297. Serialize and Deserialize Binary Tree][Accepted]committed by LeetCode Extension

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.java
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.java
@@ -11,64 +11,65 @@ public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        String res = "";
-        if(root==null){
-            return "{}";
-        }
-        ArrayList<TreeNode> queue = new ArrayList(); 
+        if(root==null)  return "{}";
+        ArrayList<TreeNode> queue = new ArrayList();
         queue.add(root);
         for(int i=0; i<queue.size(); i++){
-            TreeNode tmp = queue.get(i);
-            if(tmp==null){
+            if(queue.get(i)!=null){
+                queue.add(queue.get(i).left);
+                queue.add(queue.get(i).right);
+            }
+        }
+        while(queue.get(queue.size()-1)==null)  queue.remove(queue.size()-1);
+        
+        String encode ="";
+        for(int i=0; i<queue.size(); i++){
+            if(i==0) {
+                encode += "{"+queue.get(i).val;
                 continue;
             }
-            queue.add(tmp.left);
-            queue.add(tmp.right);
-        }
-        
-        while(queue.get(queue.size()-1)==null){
-            queue.remove(queue.size()-1);
-        }
-        
-        res += "{"+queue.get(0).val;
-        for(int i=1; i<queue.size(); i++){
-            if(queue.get(i)==null){
-                res += ",#";
-            }else{
-                res += ","+queue.get(i).val;
+            if(queue.get(i)==null) {
+                encode += ",#";
+                continue;
             }
+            encode += ","+queue.get(i).val;
         }
-        res += "}";
-        return res;
+        encode += "}";
+        return encode;
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        if(data=="{}")  return null;
+        String decode = data.substring(1, data.length()-1);
+        String[] strs = decode.split(",");
+        //if(strs[0]=="#")  return null;
+        
         ArrayList<TreeNode> queue = new ArrayList();
-        String vals[] = data.substring(1, data.length()-1).split(",");
-        if(data.length()==2){
-            return null;
-        }
-        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        TreeNode root = new TreeNode(Integer.parseInt(strs[0]));
         queue.add(root);
-        int index = 0;
         boolean isLeft = true;
-        for(int i=1; i<vals.length; i++){
-            if(!vals[i].equals("#")){
-                TreeNode tmp = new TreeNode(Integer.parseInt(vals[i]));
-                if(isLeft){
-                    queue.get(index).left = tmp; 
-                }else{
-                    queue.get(index).right = tmp;
-                }
-                queue.add(tmp);
+        int index = 0;
+        
+        for(int i=1; i<strs.length; i++){
+            TreeNode curr;
+            if(strs[i].equals("#")){
+                curr = null;
+            }else{
+                curr = new TreeNode(Integer.parseInt(strs[i]));
             }
-            if(!isLeft){
+            
+            if(isLeft){
+                queue.get(index).left = curr;
+                isLeft = false;
+            }else{
+                queue.get(index).right = curr;
+                isLeft = true;
                 index++;
             }
-            isLeft = !isLeft;
-       }
-       return root;
+            if(curr!=null)  queue.add(curr);
+        }
+        return root;
     }
 }
 
