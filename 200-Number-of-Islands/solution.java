dbfc49f344c69ec8312a46829d69fccd02b36f89
JimmyXie123 <xiezj12345@gmail.com>
2016-08-23 01:59:08 -0400

    [200. Number of Islands][Accepted]committed by LeetCode Extension

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,41 +1,53 @@
-public class Solution {
-    private int[] dx = {1, 0, 0, -1};
-    private int[] dy = {0, 1, -1, 0};
-    int m, n;
-    private void removeIsland(char[][] grid, int x, int y){
-        grid[x][y]='0';
-        for(int i=0; i<4; i++){
-            int nextX = x+dx[i];
-            int nextY = y+dy[i];
-            if(nextX>=0 && nextX<m && nextY>=0&& nextY<n){
-                if(grid[nextX][nextY]=='1'){
-                    removeIsland(grid, nextX, nextY);
-                }
-            }
-        }
+class Node{
+    int x;
+    int y;
+    public Node(int x, int y){
+        this.x = x;
+        this.y = y;
     }
-    
+}
+
+public class Solution {
+    int dX[] = {-1, 0, 0, 1};
+    int dY[] = {0, -1, 1, 0};
+    int m, n;
     public int numIslands(char[][] grid) {
-        if(grid==null||grid.length==0){
+        if(grid==null||grid.length==0||grid[0].length==0){
             return 0;
         }
-        m = grid.length;
-        if(grid[0].length==0){
-            return 0;
-        }
-        n = grid[0].length;
         
-        int count = 0;
+        m = grid.length;
+        n = grid[0].length;
+        int number = 0;
+        
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    removeIsland(grid, i, j);
+                if(grid[i][j]=='0') continue;
+                else{
+                    number++;
+                    BFS(grid, i, j);
                 }
             }
         }
         
-        return count;
-        
+        return number;
+    }
+    
+    private void BFS(char[][] grid, int x, int y){
+        Queue<Node> queue = new LinkedList();
+        queue.offer(new Node(x, y));
+        grid[x][y]='0';
+        while(!queue.isEmpty()){
+            Node curr = queue.poll();
+            for(int i=0; i<4; i++){
+                int nextX = dX[i]+curr.x;
+                int nextY = dY[i]+curr.y;
+                if(nextX>=0&&nextX<m&&nextY>=0&&nextY<n&&grid[nextX][nextY]=='1'){
+                    grid[nextX][nextY] = '0';
+                    queue.offer(new Node(nextX, nextY));
+                }
+            }
+        }
     }
 }
