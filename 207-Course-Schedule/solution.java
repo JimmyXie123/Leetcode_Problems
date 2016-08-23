ea6218a47ed541b4f3ceded83e2e0b2b4dc29750
JimmyXie123 <xiezj12345@gmail.com>
2016-08-23 16:16:04 -0400

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,12 +1,9 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int m = prerequisites.length;
-        
-        int[] inDegree = new int[numCourses];
-        for(int i=0; i<m; i++){
+        int inDegree[] = new int[numCourses];
+        for(int i=0; i<prerequisites.length; i++){
             inDegree[prerequisites[i][0]]++;
         }
-        
         
         Queue<Integer> queue = new LinkedList();
         for(int i=0; i<numCourses; i++){
@@ -16,24 +13,18 @@ public class Solution {
         }
         
         while(!queue.isEmpty()){
-            Integer number = queue.poll();
-            for(int i=0; i<m; i++){
-                if(prerequisites[i][1]==number.intValue()){
+            int curr = queue.poll();
+            for(int i=0; i<prerequisites.length; i++){
+                if(prerequisites[i][1]==curr){
                     inDegree[prerequisites[i][0]]--;
-                    if(inDegree[prerequisites[i][0]]==0){
-                        queue.offer(prerequisites[i][0]);
-                    }
+                    if(inDegree[prerequisites[i][0]]==0) queue.offer(prerequisites[i][0]);
                 }
             }
         }
         
         for(int i=0; i<numCourses; i++){
-            if(inDegree[i]!=0){
-                return false;
-            }
+            if(inDegree[i]!=0)  return false;
         }
-        
         return true;
-        
     }
 }
