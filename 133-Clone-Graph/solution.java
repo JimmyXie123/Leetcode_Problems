6581cf2c8473b3de5ef42498544670ebba6b9064
JimmyXie123 <xiezj12345@gmail.com>
2016-08-23 01:05:07 -0400

    [133. Clone Graph][Accepted]committed by LeetCode Extension

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -8,42 +8,38 @@
  */
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if(node==null)  return null;
         ArrayList<UndirectedGraphNode> nodes = getNodes(node);
-        HashMap<UndirectedGraphNode, UndirectedGraphNode> result = new HashMap();
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap();
         for(UndirectedGraphNode n:nodes){
-            result.put(n, new UndirectedGraphNode(n.label));
+            map.put(n, new UndirectedGraphNode(n.label));
         }
         
         for(UndirectedGraphNode n:nodes){
-            for(UndirectedGraphNode w:n.neighbors){
-                UndirectedGraphNode temp = result.get(w);
-                result.get(n).neighbors.add(temp);
+            for(UndirectedGraphNode neighbor:n.neighbors){
+                map.get(n).neighbors.add(map.get(neighbor));
             }
         }
-        
-        return result.get(node);
+        return map.get(node);
     }
     
     private ArrayList<UndirectedGraphNode> getNodes(UndirectedGraphNode node){
+        Queue<UndirectedGraphNode> queue = new LinkedList();
         ArrayList<UndirectedGraphNode> nodes = new ArrayList();
         HashSet<UndirectedGraphNode> set = new HashSet();
-        if(node==null){
-            return nodes;
-        }
-        Queue<UndirectedGraphNode> queue = new LinkedList();
+        
         queue.offer(node);
         set.add(node);
+        
         while(!queue.isEmpty()){
-            UndirectedGraphNode temp = queue.poll();
-            //if(!set.contains(temp)){
-            //set.add(temp);
-            nodes.add(temp);
-            //}
-            for(UndirectedGraphNode n:temp.neighbors){
-                if(!set.contains(n)){
-                    queue.offer(n);
-                    set.add(n);
+            UndirectedGraphNode curr = queue.poll();
+            nodes.add(curr);
+            for(UndirectedGraphNode n:curr.neighbors){
+                if(set.contains(n)){
+                    continue;
                 }
+                queue.offer(n);
+                set.add(n);
             }
         }
         return nodes;
