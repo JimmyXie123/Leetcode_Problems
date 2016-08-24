0a27e2ac0f6ac137ae8fbeaafe663f291923e1c8
JimmyXie123 <xiezj12345@gmail.com>
2016-08-24 16:05:02 -0400

    [329. Longest Increasing Path in a Matrix][Accepted]committed by LeetCode Extension

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -1,39 +1,34 @@
 public class Solution {
-    private int[] dX={-1, 0, 0, 1};
-    private int[] dY={0, -1, 1, 0};
-    int m,n;
+    int dX[] = {-1, 0, 0, 1};
+    int dY[] = {0, -1, 1, 0};
+    int m, n;
     public int longestIncreasingPath(int[][] matrix) {
-        if(matrix==null||matrix.length==0){
-            return 0;
-        }
+        if(matrix==null||matrix.length==0||matrix[0].length==0) return 0;
         m = matrix.length;
-        if(matrix[0].length==0){
-            return 0;
-        }
         n = matrix[0].length;
+        int cache[][] = new int[m][n];
         
-        int[][] cache = new int[m][n]; 
-        int max = 1;
+        int max = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 max = Math.max(dfs(matrix, i, j, cache), max);
             }
         }
-        
         return max;
     }
     
-    private int dfs(int[][] matrix, int i, int j, int[][] cache){
-        if(cache[i][j]!=0) return cache[i][j]; 
+    private int dfs(int[][] matrix, int x, int y, int[][] cache){
+        if(cache[x][y]!=0)  return cache[x][y];
         int max = 1;
-        for(int k=0; k<4; k++){
-            int nextX = i+dX[k];
-            int nextY = j+dY[k];
-            if(nextX>=0 && nextX<m && nextY>=0 && nextY<n && matrix[i][j]<matrix[nextX][nextY]){
-                max = Math.max(max, 1+dfs(matrix, nextX, nextY, cache));
+        for(int i=0; i<4; i++){
+            int nextX = x+dX[i];
+            int nextY = y+dY[i];
+            if(nextX>=0 && nextX<m && nextY>=0 && nextY<n && matrix[x][y]<matrix[nextX][nextY]){
+                max = Math.max(dfs(matrix, nextX, nextY, cache)+1, max);
             }
         }
-        cache[i][j] = max;
+        cache[x][y] = max;
         return max;
     }
 }
+
