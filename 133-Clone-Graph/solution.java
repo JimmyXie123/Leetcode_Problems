94acc185e17c82f85a109a71fb1108dca1e3f004
JimmyXie123 <xiezj12345@gmail.com>
2016-08-24 11:22:00 -0400

    [133. Clone Graph][Accepted]committed by LeetCode Extension

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -7,41 +7,16 @@
  * };
  */
 public class Solution {
+    HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap();
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if(node==null)  return null;
-        ArrayList<UndirectedGraphNode> nodes = getNodes(node);
-        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap();
-        for(UndirectedGraphNode n:nodes){
-            map.put(n, new UndirectedGraphNode(n.label));
+        UndirectedGraphNode curr = new UndirectedGraphNode(node.label);
+        map.put(node, curr);
+        for(UndirectedGraphNode neighbor:node.neighbors){
+            if(!map.containsKey(neighbor)) curr.neighbors.add(cloneGraph(neighbor));
+            else curr.neighbors.add(map.get(neighbor));
         }
         
-        for(UndirectedGraphNode n:nodes){
-            for(UndirectedGraphNode neighbor:n.neighbors){
-                map.get(n).neighbors.add(map.get(neighbor));
-            }
-        }
-        return map.get(node);
-    }
-    
-    private ArrayList<UndirectedGraphNode> getNodes(UndirectedGraphNode node){
-        Queue<UndirectedGraphNode> queue = new LinkedList();
-        ArrayList<UndirectedGraphNode> nodes = new ArrayList();
-        HashSet<UndirectedGraphNode> set = new HashSet();
-        
-        queue.offer(node);
-        set.add(node);
-        
-        while(!queue.isEmpty()){
-            UndirectedGraphNode curr = queue.poll();
-            nodes.add(curr);
-            for(UndirectedGraphNode n:curr.neighbors){
-                if(set.contains(n)){
-                    continue;
-                }
-                queue.offer(n);
-                set.add(n);
-            }
-        }
-        return nodes;
+        return curr;
     }
 }
