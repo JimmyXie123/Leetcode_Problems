11a25079cc69aa61866ab428cc4c42987cfee966
JimmyXie123 <xiezj12345@gmail.com>
2016-08-24 11:33:05 -0400

    [133. Clone Graph][Accepted]committed by LeetCode Extension

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -7,16 +7,15 @@
  * };
  */
 public class Solution {
-    HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap();
+    HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap(); 
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if(node==null)  return null;
         UndirectedGraphNode curr = new UndirectedGraphNode(node.label);
         map.put(node, curr);
         for(UndirectedGraphNode neighbor:node.neighbors){
-            if(!map.containsKey(neighbor)) curr.neighbors.add(cloneGraph(neighbor));
+            if(!map.containsKey(neighbor))  curr.neighbors.add(cloneGraph(neighbor));
             else curr.neighbors.add(map.get(neighbor));
         }
-        
         return curr;
     }
 }
