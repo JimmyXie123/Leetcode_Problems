e9e09e87a80b0a095defc23f74472a0ba796f8fd
JimmyXie123 <xiezj12345@gmail.com>
2016-08-24 16:42:08 -0400

    [364. Nested List Weight Sum II][Accepted]committed by LeetCode Extension

--- 364-Nested-List-Weight-Sum-II/solution.java
+++ 364-Nested-List-Weight-Sum-II/solution.java
@@ -2,6 +2,11 @@
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
  * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
  *
  *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
  *     public boolean isInteger();
@@ -10,39 +15,43 @@
  *     // Return null if this NestedInteger holds a nested list
  *     public Integer getInteger();
  *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
  *     // @return the nested list that this NestedInteger holds, if it holds a nested list
  *     // Return null if this NestedInteger holds a single integer
  *     public List<NestedInteger> getList();
  * }
  */
 public class Solution {
+    int level = 0;
     int sum = 0;
     public int depthSumInverse(List<NestedInteger> nestedList) {
-        if(nestedList==null){
-            return 0;
-        }
-        sum(nestedList, getLevel(nestedList));
+        for(NestedInteger n:nestedList) getDepth(n, 0);
+        for(NestedInteger n:nestedList) dfs(n, level);
         return sum;
     }
     
-    private int getLevel(List<NestedInteger> list){
-        int res = 0;
-        for(NestedInteger item:list){
-            if(item.isInteger()){
-                res = Math.max(1, res);
-            }else{
-                res = Math.max(getLevel(item.getList())+1, res);
+    private void getDepth(NestedInteger element, int depth){
+        if(element.isInteger()){
+            level = Math.max(depth+1, level);
+            return;
+        }else{
+            for(NestedInteger sub:element.getList()){
+                getDepth(sub, depth+1);
             }
         }
-        return res;
     }
     
-    private void sum(List<NestedInteger> list, int level){
-        for(NestedInteger item:list){
-            if(item.isInteger()){
-                sum += level*item.getInteger();
-            }else{
-                sum(item.getList(), level-1);
+    private void dfs(NestedInteger element, int depth){
+        if(element.isInteger()){
+            sum += element.getInteger()*depth;
+        }else{
+            for(NestedInteger sub:element.getList()){
+                dfs(sub, depth-1);
             }
         }
     }
