0586cbd699e8d1d9b445e21448f50f38e6719352
JimmyXie123 <xiezj12345@gmail.com>
2016-08-25 15:39:02 -0400

    [301. Remove Invalid Parentheses][Accepted]committed by LeetCode Extension

--- 301-Remove-Invalid-Parentheses/solution.java
+++ 301-Remove-Invalid-Parentheses/solution.java
@@ -1,27 +1,30 @@
 public class Solution {
     public List<String> removeInvalidParentheses(String s) {
-        List<String> ans = new ArrayList();
-        remove(s, ans, 0, 0, new char[]{'(', ')'});
-        return ans;
+        List<String> res = new ArrayList();
+        remove(s, 0, 0, new char[]{'(', ')'}, res);
+        return res;
     }
     
-    private void remove(String s, List<String> ans, int last_i, int last_j, char[] par){
-        for(int i=last_i, stack=0; i<s.length(); i++){
-            if(s.charAt(i)==par[0]) stack++;
-            if(s.charAt(i)==par[1]) stack--;
+    private void remove(String str, int last_i, int last_j, char[] ch, List<String> res){
+        for(int i=last_i, stack=0; i<str.length(); i++){
+            if(str.charAt(i)==ch[0]) stack++;
+            if(str.charAt(i)==ch[1]) stack--;
             if(stack>=0) continue;
+            
             for(int j=last_j; j<=i; j++){
-                if( (j==last_j||s.charAt(j-1)!=par[1])&&(s.charAt(j)==par[1])){
-                    remove(s.substring(0, j)+s.substring(j+1, s.length()), ans, i, j, par);
+                if((j==last_j||str.charAt(j-1)!=ch[1])&&str.charAt(j)==ch[1]){
+                    remove(str.substring(0, j)+str.substring(j+1, str.length()), i, j, ch, res);
                 }
             }
             return;
         }
-        String reverse = new StringBuilder(s).reverse().toString();
-        if(par[0]=='('){
-            remove(reverse, ans, 0, 0, new char[]{')', '('});
+        
+        String reverse = new StringBuilder(str).reverse().toString(); 
+        if(ch[0]=='('){
+            remove(reverse, 0, 0, new char[]{')', '('}, res);
         }else{
-            ans.add(reverse);
+            res.add(reverse);
         }
-    }
+    }    
+    
 }
