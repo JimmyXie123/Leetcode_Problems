be8c4c094e2e4771cd3711f10016ea11625cf0ba
JimmyXie123 <xiezj12345@gmail.com>
2016-08-25 22:01:57 -0400

    [332. Reconstruct Itinerary][Accepted]committed by LeetCode Extension

--- 332-Reconstruct-Itinerary/solution.java
+++ 332-Reconstruct-Itinerary/solution.java
@@ -2,21 +2,22 @@ public class Solution {
     public List<String> findItinerary(String[][] tickets) {
         HashMap<String, PriorityQueue<String>> map = new HashMap();
         for(String[] ticket:tickets){
-            if(!map.containsKey(ticket[0])) map.put(ticket[0], new PriorityQueue());
-            map.get(ticket[0]).offer(ticket[1]);
+            if(!map.containsKey(ticket[0])){
+                map.put(ticket[0], new PriorityQueue());
+            }
+            map.get(ticket[0]).add(ticket[1]);
         }
         
         List<String> res = new ArrayList();
-        helper(map, "JFK", res);
+        helper(map, res, "JFK");
         Collections.reverse(res);
         return res;
     }
     
-    private void helper(HashMap<String, PriorityQueue<String>> map, String str, List<String> res){
+    private void helper(HashMap<String, PriorityQueue<String>> map, List<String> res, String str){
         while(map.containsKey(str)&&!map.get(str).isEmpty()){
-            helper(map, map.get(str).poll(), res);
+            helper(map, res, map.get(str).poll());
         }
         res.add(str);
-        
     }
 }
