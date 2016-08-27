2d1c6924a087944823635e274df279bda0b8513e
JimmyXie123 <xiezj12345@gmail.com>
2016-08-26 22:10:50 -0400

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,41 +1,18 @@
-//-------------同combination sum
 public class Solution {
-    int min = Integer.MAX_VALUE;
     public int numSquares(int n) {
-        int num = 0;
-        //int[] cache = new int[n];
-        
-        while(num*num<=n){
-            num++;
-        }
-        num--;
-        List<Integer> nums = new ArrayList();
-        for(int i=num; i>=1; i--){
-            nums.add(i*i);
-        }
-        
-        helper(nums, 0, n, 0);
-        return min;
-    }
-    
-    private void helper(List<Integer> nums, int pos, int sum, int count){
-        if(count>min){
-            return;
-        }
-        
-        if(sum<0){
-            return;
-        }
-        if(sum==0){
-            if(count<min){
-                min = count;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int[] res = new int[n+1];
+        res[1] = 1;
+        for(int i=2; i<=n; i++){
+            for(int j=i/2; j>=1; j--){
+                if(i-j*j>=0){
+                    if(res[i]==0) res[i]=res[i-j*j]+1;
+                    else res[i] = Math.min(res[i], res[i-j*j]+1);
+                }
             }
         }
         
-        for(int i= pos; i<nums.size(); i++){
-            count++;
-            helper(nums, i, sum-nums.get(i), count);
-            count--;
-        }
+        return res[n];
     }
 }
