f5c81e56a83fc03a4328cd54f11522fdd42f1461
JimmyXie123 <xiezj12345@gmail.com>
2016-08-27 00:53:29 -0400

    [279. Perfect Squares][Accepted]committed by LeetCode Extension

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -1,24 +1,12 @@
 public class Solution {
     public int numSquares(int n) {
-        Queue<Integer> queue = new LinkedList();
         int[] dp = new int[n+1];
-        for(int i=1; i*i<=n; i++){
-            if(i*i==n)  return 1;
-            queue.offer(i*i);
-            dp[i*i] = 1;
-        }
-        
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            for(int i=1; i*i<=n-curr; i++){
-                if(i*i==n-curr){
-                    return dp[curr]+1;
-                }else if(i*i<n-curr&&dp[i*i+curr]==0){
-                    dp[i*i+curr] = dp[curr]+1;
-                    queue.offer(i*i+curr);
-                }else if(i*i+curr>n){
-                    break;
-                }
+        if(n==0) return 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            for(int j=1; j<=i/2; j++){
+                if(dp[i]==0&&i-j*j>=0) dp[i] = dp[i-j*j]+1;
+                else if(dp[i]!=0&&i-j*j>=0) dp[i] = Math.min(dp[i-j*j]+1, dp[i]);
             }
         }
         
