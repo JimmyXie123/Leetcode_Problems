772048b6fd9700ac9a96011d8cf2db6a134d520c
JimmyXie123 <xiezj12345@gmail.com>
2016-08-28 11:23:11 -0400

    [286. Walls and Gates][Accepted]committed by LeetCode Extension

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -1,22 +1,39 @@
 public class Solution {
+    int dX[] = {-1, 0, 0, 1};
+    int dY[] = {0, -1, 1, 0};
     int m, n;
     public void wallsAndGates(int[][] rooms) {
         if(rooms==null||rooms.length==0||rooms[0].length==0) return;
+        Queue<Integer> queue = new LinkedList();
         m = rooms.length;
         n = rooms[0].length;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(rooms[i][j]==0)  helper(rooms, i, j, 0);
+                if(rooms[i][j]==0) queue.offer(getNum(i, j));
+            }
+        }
+        
+        int level = 0;
+        while(!queue.isEmpty()){
+            level++;
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                int curr = queue.poll();
+                int x = curr/n;
+                int y = curr%n;
+                for(int k=0; k<4; k++){
+                    int nextX = x+dX[k];
+                    int nextY = y+dY[k];
+                    if(nextX>=0&&nextX<m&&nextY<n&&nextY>=0&&rooms[nextX][nextY]>level){
+                        rooms[nextX][nextY] = level;
+                        queue.offer(getNum(nextX, nextY));
+                    }
+                }
             }
         }
     }
     
-    private void helper(int[][] rooms, int x, int y, int d){
-        if(x<0||x>=m||y<0||y>=n||rooms[x][y]<d)   return;
-        rooms[x][y] = d;
-        helper(rooms, x+1, y, d+1);
-        helper(rooms, x, y+1, d+1);
-        helper(rooms, x-1, y, d+1);
-        helper(rooms, x, y-1, d+1);
+    private int getNum(int x, int y){
+        return x*n+y;
     }
 }
