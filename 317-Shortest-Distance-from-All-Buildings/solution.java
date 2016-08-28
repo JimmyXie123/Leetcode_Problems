5ba83519691af1a062b6446270866586168444bb
JimmyXie123 <xiezj12345@gmail.com>
2016-08-28 17:47:18 -0400

    [317. Shortest Distance from All Buildings][Accepted]committed by LeetCode Extension

--- 317-Shortest-Distance-from-All-Buildings/solution.java
+++ 317-Shortest-Distance-from-All-Buildings/solution.java
@@ -3,77 +3,75 @@ public class Solution {
     int dY[] = {0, -1, 1, 0};
     int m, n;
     public int shortestDistance(int[][] grid) {
+        if(grid==null||grid.length==0||grid[0].length==0)  return 0;
         m = grid.length;
         n = grid[0].length;
-        int houseCount =0;
+        int houseCount = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1)   houseCount++; 
+            }
+        }
+        
+        
+        boolean flag = false;
+        int res=Integer.MAX_VALUE;
+        int[][] reachCount = new int[m][n];
+        int[][] distance = new int[m][n];
+        
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==1){
-                    houseCount++;
+                    bfs(grid, i, j, reachCount, distance, houseCount);
                 }
             }
         }
         
-        int reachCounts[][] = new int[m][n];
-        int distance[][] = new int[m][n];
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j]==1){
-                    int tmp = bfs(grid, i, j, reachCounts, distance);
-                    if(tmp!=houseCount)    return -1;
-                }
-            }
-        }
         
-        int res = Integer.MAX_VALUE;;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]==0&&reachCounts[i][j]==houseCount){
+                if(reachCount[i][j]==houseCount){
+                    flag = true;
                     res = Math.min(res, distance[i][j]);
                 }
             }
         }
-        return res;
+        
+        if(flag) return res;
+        else return -1;
     }
     
-    private int bfs(int[][] grid, int x, int y, int[][] reachCounts, int[][] distance){
-        boolean visited[][] = new boolean[grid.length][grid[0].length];
-        int count = 0;
-        int level = -1;
-        Queue<Node> queue = new LinkedList();
-        queue.offer(new Node(x, y));
+    private void bfs(int[][] grid, int x, int y, int[][] reachCount, int[][] distance, int houseCount){
+        boolean[][] visited = new boolean[m][n];
+        Queue<Integer> queue = new LinkedList();
+        queue.offer(getNum(x, y));
+        int level = 0;
+        visited[x][y] = true;
+        
         while(!queue.isEmpty()){
             int size = queue.size();
             level++;
-            for(int j=0; j<size; j++){
-                Node tmp = queue.poll();
-                if(grid[tmp.x][tmp.y]==0)  distance[tmp.x][tmp.y] += level;
-                for(int i=0; i<4; i++){
-                    int nextX = tmp.x+dX[i];
-                    int nextY = tmp.y+dY[i];
-                    if(nextX<0||nextX>=m||nextY<0||nextY>=n||grid[nextX][nextY]==2||visited[nextX][nextY]){
-                        continue;
+            for(int i=0; i<size; i++){
+                int curr = queue.poll();
+                int curr_x = curr/n;
+                int curr_y = curr%n;
+                //System.out.println(curr_x+" "+curr_y);
+                for(int k=0; k<4; k++){
+                    int nextX = dX[k]+curr_x;
+                    int nextY = dY[k]+curr_y;
+                    if(nextX>=0&&nextX<m&&nextY>=0&&nextY<n&&!visited[nextX][nextY]&&grid[nextX][nextY]==0){
+                        reachCount[nextX][nextY]++;
+                        distance[nextX][nextY] += level;
+                        
+                        visited[nextX][nextY] = true;   //------------是个大坑-------------------
+                        queue.offer(getNum(nextX, nextY));
                     }
-                    if(grid[nextX][nextY]==1){
-                        visited[nextX][nextY]=true;
-                        count++;
-                        continue;
-                    }
-                    visited[nextX][nextY]=true;
-                    reachCounts[nextX][nextY]++;
-                    queue.offer(new Node(nextX, nextY));
                 }
             }
         }
-        return count;
     }
-}
-
-class Node{
-    int x;
-    int y;
-    public Node(int x, int y){
-        this.x = x;
-        this.y = y;
+    
+    private int getNum(int x, int y){
+        return x*n+y;
     }
 }
