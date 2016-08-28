e1ee835e118f8234563adbe15b41a6aef016e720
JimmyXie123 <xiezj12345@gmail.com>
2016-08-28 11:10:34 -0400

    [286. Walls and Gates][Accepted]committed by LeetCode Extension

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -1,18 +1,22 @@
 public class Solution {
+    int m, n;
     public void wallsAndGates(int[][] rooms) {
-        for(int i=0; i<rooms.length; i++){
-            for(int j=0; j<rooms[0].length; j++){
-                if(rooms[i][j]==0) helper(rooms, i, j, 0);
+        if(rooms==null||rooms.length==0||rooms[0].length==0) return;
+        m = rooms.length;
+        n = rooms[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(rooms[i][j]==0)  helper(rooms, i, j, 0);
             }
         }
     }
     
-    private void helper(int[][] rooms, int i, int j, int d){
-        if(i<0||i>=rooms.length||j<0||j>=rooms[0].length||rooms[i][j]<d) return;
-        rooms[i][j] = d;
-        helper(rooms, i, j+1, d+1);
-        helper(rooms, i, j-1, d+1);
-        helper(rooms, i-1, j, d+1);
-        helper(rooms, i+1, j, d+1);
+    private void helper(int[][] rooms, int x, int y, int d){
+        if(x<0||x>=m||y<0||y>=n||rooms[x][y]<d)   return;
+        rooms[x][y] = d;
+        helper(rooms, x+1, y, d+1);
+        helper(rooms, x, y+1, d+1);
+        helper(rooms, x-1, y, d+1);
+        helper(rooms, x, y-1, d+1);
     }
 }
