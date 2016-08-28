5f47fd39d591ca3906ddc3fad80ed76911133dc9
JimmyXie123 <xiezj12345@gmail.com>
2016-08-28 10:42:05 -0400

    [310. Minimum Height Trees][Accepted]committed by LeetCode Extension

--- 310-Minimum-Height-Trees/solution.java
+++ 310-Minimum-Height-Trees/solution.java
@@ -1,56 +1,53 @@
 public class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        HashMap<Integer, Node> graph = new HashMap();
+        HashMap<Integer, Node> graph = new HashMap();   //---------for adding neighbors-------------
         for(int i=0; i<n; i++){
             graph.put(i, new Node(i));
         }
         
-        for(int[] e:edges){
-            graph.get(e[0]).neighbors.add(graph.get(e[1]));
-            graph.get(e[1]).neighbors.add(graph.get(e[0]));
-            graph.get(e[0]).degree++;
-            graph.get(e[1]).degree++;
+        for(int[] edge:edges){
+            graph.get(edge[0]).neighbors.add(graph.get(edge[1]));
+            graph.get(edge[1]).neighbors.add(graph.get(edge[0]));
+            graph.get(edge[0]).degree++;
+            graph.get(edge[1]).degree++;
         }
         
         Queue<Node> queue = new LinkedList();
         for(int i=0; i<n; i++){
-            if(graph.get(i).degree==1){
-                queue.offer(graph.get(i));
-            }
+            if(graph.get(i).degree==1)  queue.offer(graph.get(i));
         }
         
         while(n>2){
             int size = queue.size();
             for(int i=0; i<size; i++){
-                Node temp = queue.poll();
+                Node tmp = queue.poll();
+                graph.remove(tmp.label);
                 n--;
-                graph.remove(temp.label);
-                for(Node side:temp.neighbors){
-                    side.neighbors.remove(temp);
-                    side.degree--;
-                    if(side.degree==1){
+                for(Node side:tmp.neighbors){
+                    graph.get(side.label).degree--;
+                    graph.get(side.label).neighbors.remove(tmp);
+                    if(graph.get(side.label).degree==1){
                         queue.offer(side);
                     }
                 }
             }
         }
         
-        List<Integer> result = new ArrayList();
-        for(Integer i:graph.keySet()){
-            result.add(i);
+        List<Integer> res = new ArrayList();
+        for(int i:graph.keySet()){
+            res.add(i);
         }
-        return result;
+        return res;
     }
-    
 }
 
 class Node{
-    int label;
     int degree;
+    int label;
     HashSet<Node> neighbors;
     public Node(int label){
-        this.label = label;
         degree = 0;
-        neighbors = new HashSet(); 
+        this.label = label;
+        neighbors = new HashSet();
     }
 }
