4e0dd11a9b3bf6610034608b44659b0ef6e75da5
JimmyXie123 <xiezj12345@gmail.com>
2016-08-29 12:36:48 -0400

    [294. Flip Game II][Accepted]committed by LeetCode Extension

--- 294-Flip-Game-II/solution.java
+++ 294-Flip-Game-II/solution.java
@@ -1,28 +1,26 @@
 public class Solution {
     public boolean canWin(String s) {
-        boolean state[] = new boolean[s.length()];
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='+'){
-                state[i] = true;
-            }
+        int len = s.length();
+        boolean[] state = new boolean[len];
+        for(int i=0; i<len; i++){
+            if(s.charAt(i)=='+')  state[i] = true;
         }
         
         return helper(state);
-        
     }
     
     private boolean helper(boolean[] state){
         for(int i=0; i<state.length-1; i++){
             if(state[i]&&state[i+1]){
-                state[i] = false;
-                state[i+1] = false;
-                if(!helper(state)){
-                    state[i] = true;
-                    state[i+1] = true;
+                state[i] = !state[i];
+                state[i+1] = !state[i+1];
+                if(!helper(state))  {
+                    state[i] = !state[i];
+                    state[i+1] = !state[i+1];
                     return true;
                 }
-                state[i] = true;
-                state[i+1] = true;
+                state[i] = !state[i];
+                state[i+1] = !state[i+1];
             }
         }
         return false;
