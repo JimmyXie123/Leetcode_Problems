0954b446b34f026d80b792022a94bc8502c88d5d
JimmyXie123 <xiezj12345@gmail.com>
2016-08-29 12:10:11 -0400

    [357. Count Numbers with Unique Digits][Accepted]committed by LeetCode Extension

--- 357-Count-Numbers-with-Unique-Digits/solution.java
+++ 357-Count-Numbers-with-Unique-Digits/solution.java
@@ -1,36 +1,35 @@
 public class Solution {
-    public static int countNumbersWithUniqueDigits(int n) {
-		int count = 1; // x == 0
-		long max = (long) Math.pow(10, n);
-
-		boolean[] used = new boolean[10];
-
-		for (int i = 1; i < 10; i++) {
-			used[i] = true;
-			count += search(i, max, used);
-			used[i] = false;
-		}
-
-		return count;
-	}
-
-	private static int search(long prev, long max, boolean[] used) {
-		int count = 0;
-		if (prev < max) {
-			count += 1;
-		} else {
-			return count;
-		}
-
-		for (int i = 0; i < 10; i++) {
-			if (!used[i]) {
-				used[i] = true;
-				long cur = 10 * prev + i;
-				count += search(cur, max, used);
-				used[i] = false;
-			}
-		}
-
-		return count;
-	}
+    public int countNumbersWithUniqueDigits(int n) {
+        long max = (long)Math.pow(10, n);
+        boolean[] used = new boolean[10];
+        if(n==0)    return 1;
+        
+        
+        int res = 1;
+        for(int i=1; i<10; i++){
+            used[i] = true;
+            res += helper(i, max, used);
+            used[i] = false;
+        }
+        return res;
+    }
+    
+    private int helper(long prev, long max, boolean[] used){
+        int count = 0;
+        if(prev<max){
+            count++;
+        }else{
+            return count;
+        }
+        
+        for(int i=0; i<10; i++){
+            if(!used[i]){
+                used[i] = true;
+                long curr = prev*10+i;
+                count += helper(curr, max, used);
+                used[i] = false;
+            }
+        }
+        return count;
+    }
 }
