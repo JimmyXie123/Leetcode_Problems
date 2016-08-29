08d0f9bdb48523c0c5966583a9b13fddcc416ce8
JimmyXie123 <xiezj12345@gmail.com>
2016-08-29 15:49:16 -0400

    [320. Generalized Abbreviation][Accepted]committed by LeetCode Extension

--- 320-Generalized-Abbreviation/solution.java
+++ 320-Generalized-Abbreviation/solution.java
@@ -10,13 +10,14 @@ public class Solution {
         String tmp="";
         int count = 0;
         int k=0;
-        while(k<path.length){    //------------跳格的都得用while-----------------
+        int len = path.length;
+        while(k<len){    //------------跳格的都得用while-----------------
             if(path[k]!='1'){
                 tmp += path[k];
                 k++;
             }else{
                 int pointer = 0;
-                while(k+pointer<path.length&&path[pointer+k]=='1'){
+                while(k+pointer<len&&path[pointer+k]=='1'){
                     pointer++;
                     count++;
                 }
@@ -26,11 +27,11 @@ public class Solution {
         }
         res.add(tmp);
         
-        if(pos==path.length){
+        if(pos==len){
             return;
         }
         
-        for(int i=pos; i<path.length; i++){
+        for(int i=pos; i<len; i++){
             char temp = path[i];
             path[i] = '1';
             DFS(res, path, word, i+1);
