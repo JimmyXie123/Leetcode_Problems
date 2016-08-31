6d2084cc487a3b5e32ef2440dcff73422cafc961
JimmyXie123 <xiezj12345@gmail.com>
2016-08-31 16:47:10 -0400

    [36. Valid Sudoku][Accepted]committed by LeetCode Extension

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -1,56 +1,20 @@
 public class Solution {
-    int[] dX = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
-    int[] dY = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
     public boolean isValidSudoku(char[][] board) {
-        boolean[] visited = new boolean[9];
-    
-        //every row
         for(int i=0; i<9; i++){
-            Arrays.fill(visited, false);
+            HashSet<Character> rows = new HashSet();
+            HashSet<Character> columns = new HashSet();
+            HashSet<Character> grid = new HashSet();
             for(int j=0; j<9; j++){
-                if(!process(visited, board, i, j)){
-                    return false;
-                }
+                if(board[i][j]!='.'&&!rows.add(board[i][j]))   return false;
+                if(board[j][i]!='.'&&!columns.add(board[j][i]))   return false;
+                int rowstart = i/3;
+                int colstart = i%3;
+                int rowindex = j/3;
+                int colindex = j%3;
+                if(board[rowstart*3+rowindex][3*colstart+colindex]!='.'&&!grid.add(board[rowstart*3+rowindex][3*colstart+colindex]))    return false;
+                
             }
         }
-        
-        //every col
-        for(int i=0; i<9; i++){
-            Arrays.fill(visited, false);
-            for(int j=0; j<9; j++){
-                if(!process(visited, board, j, i)){
-                    return false;
-                }
-            }
-        }
-        
-        System.out.println("123");
-        //subgrid
-        for(int i=1; i<9; i+= 3){
-            for(int j=1; j<9; j+= 3){
-                Arrays.fill(visited, false);
-                for(int w=0; w<9; w++){
-                    int nextX = i+dX[w];
-                    int nextY = j+dY[w];
-                    if(!process(visited, board, nextX, nextY)){
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    
-    }
-    
-    private boolean process(boolean[] visited, char[][] board, int i, int j){
-        if(board[i][j]=='.'){
-            return true;
-        }
-        int tmp =board[i][j]-'0';
-        if(tmp<=0||tmp>=10||visited[tmp-1]){
-            return false;
-        }
-        visited[tmp-1] = true; 
         return true;
     }
 }
