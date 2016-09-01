8db2141c14dbb583c5a7bbc00838ac33556e57e7
JimmyXie123 <xiezj12345@gmail.com>
2016-08-31 21:10:49 -0400

    [37. Sudoku Solver][Accepted]committed by LeetCode Extension

--- 37-Sudoku-Solver/solution.java
+++ 37-Sudoku-Solver/solution.java
@@ -1,58 +1,37 @@
 public class Solution {
-    int[] dX = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
-    int[] dY = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
     public void solveSudoku(char[][] board) {
-        solver(board); 
+        boolean tmp = helper(board);
     }
     
-    private boolean solver(char[][] board){
-        for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
-                if(board[i][j]!='.'){
-                    continue;
-                }
-                for(int k=1; k<=9; k++){
-                    board[i][j] = (char)(k+'0');
-                    if(isValid(board, i, j)&&solver(board)){
-                        return true;
+    private boolean helper(char[][] board){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j]=='.'){
+                    
+                    for(char c='1'; c<='9'; c++){
+                        if(isValid(board, i, j, c)){
+                            board[i][j] = c;
+                            if(helper(board))   return true;
+                            board[i][j] = '.';
+                        }
                     }
-                    board[i][j] = '.';  //-------------think about backtracking here.----------------
+                    return false;
                 }
-                return false;
             }
         }
+        
         return true;
     }
     
-    private boolean isValid(char[][] board, int a, int b){
-        HashSet<Character> set = new HashSet(); 
-        for(int j=0; j<9; j++){
-            if(set.contains(board[a][j])){
-                return false;
-            }
-             if(board[a][j]>'0' && board[a][j]<='9')
-                set.add(board[a][j]);
-        }
-        set.clear();
-        for(int i=0; i<9; i++){
-            if(set.contains(board[i][b])){
-                return false;
-            }
-            if(board[i][b]>'0' && board[i][b]<='9')
-                set.add(board[i][b]);
+    private boolean isValid(char[][] board, int i, int j, char curr){
+        for(int k=0; k<9; k++){
+            if(board[i][k]==curr||board[k][j]==curr)  return false;
         }
         
-        set.clear();
-        int startX = a/3*3+1;
-        int startY = b/3*3+1;
+        int rowstart = i/3*3;
+        int colstart = j/3*3;
         for(int k=0; k<9; k++){
-            int nextX = startX+dX[k];
-            int nextY = startY+dY[k];
-            if(set.contains(board[nextX][nextY])){
-                return false;
-            }
-             if(board[nextX][nextY]>'0' && board[nextX][nextY]<='9')
-                set.add(board[nextX][nextY]);
+            if(board[rowstart+k/3][colstart+k%3]==curr)  return false;
         }
         return true;
     }
