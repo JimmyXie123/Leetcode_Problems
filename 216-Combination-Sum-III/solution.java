6b862fbc8981ea305954fe9c4c66825651c29c36
JimmyXie123 <xiezj12345@gmail.com>
2016-08-31 23:31:19 -0400

    [216. Combination Sum III][Accepted]committed by LeetCode Extension

--- 216-Combination-Sum-III/solution.java
+++ 216-Combination-Sum-III/solution.java
@@ -1,26 +1,24 @@
 public class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> res = new ArrayList();
-        if(n<=0||k<=0){
-            return res;
-        }
-        List<Integer> path = new ArrayList();
-        dfs(1, k, 0, n, res, path);
+        List<Integer> list = new ArrayList();
+        if(k<=0||k>9||n>45||n<=0)  return res;
+        helper(res, list, k, n, 1);
         return res;
     }
     
-    private void dfs(int pos, int k, int sum, int n, List<List<Integer>> res, List<Integer> path){
-        if(sum>n||k<0){
+    private void helper(List<List<Integer>> res, List<Integer> list, int count, int target, int pos){
+        if(count==0){
+            if(target==0)   res.add(new ArrayList(list));
             return;
         }
-        if(sum==n&&k==0){
-          res.add(new ArrayList(path));
-          return;
-        }
+        
+        if(target<=0)  return;
+        
         for(int i=pos; i<=9; i++){
-            path.add(i);
-            dfs(i+1, k-1, sum+i, n, res, path);
-            path.remove(path.size()-1);
+            list.add(i);
+            helper(res, list, count-1, target-i, i+1);
+            list.remove(list.size()-1);
         }
     }
 }
