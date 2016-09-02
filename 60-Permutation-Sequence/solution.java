8d2bea7e20978681e5fe9d0f7504af7da6f2c695
JimmyXie123 <xiezj12345@gmail.com>
2016-09-02 16:04:31 -0400

    [60. Permutation Sequence][Accepted]committed by LeetCode Extension

--- 60-Permutation-Sequence/solution.java
+++ 60-Permutation-Sequence/solution.java
@@ -1,33 +1,29 @@
 public class Solution {
     public String getPermutation(int n, int k) {
         String res = "";
+        boolean[] used =new boolean[n];
         int factor = 1;
-        boolean[] used = new boolean[n]; 
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n; i++) {
             factor *= i;
         }
-        k=k-1;
-        
+        k = k-1;
         
         for(int i=0; i<n; i++){
             int index = k/factor;
             k = k%factor;
             for(int j=0; j<n; j++){
-                if(!used[j]){
+                if(used[j]==false){
                     if(index==0){
-                        used[j]=true;
-                        res +=(char)('0'+j+1);
+                        used[j] = true;
+                        res += (char)('1'+j);
                         break;
                     }else{
                         index--;
                     }
                 }
             }
-            if(n>i+1)  factor = factor/(n-i-1);
+            if(n>i+1)   factor = factor/(n-i-1);
         }
-        
         return res;
-        
-        
     }
 }
