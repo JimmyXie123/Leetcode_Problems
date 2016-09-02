07fdcb4312095f932fd2bcb4b2ad1a8b3d588832
JimmyXie123 <xiezj12345@gmail.com>
2016-09-02 16:23:04 -0400

    [77. Combinations][Accepted]committed by LeetCode Extension

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,23 +1,21 @@
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList();
-        List<Integer> path = new ArrayList();
-        DFS(res, path, 1, n, k);
+        List<Integer> list = new ArrayList();
+        helper(res, list, 1, k, n);
         return res;
     }
     
-    private void DFS(List<List<Integer>> res, List<Integer> path, int pos, int n, int k){
-        if(path.size()==k){
-            List<Integer> tmp = new ArrayList(path);
-            Collections.sort(tmp);
-            res.add(tmp);
+    private void helper(List<List<Integer>> res, List<Integer> list, int pos, int count, int n){
+        if(count==0) {
+            res.add(new ArrayList(list));
             return;
         }
         
         for(int i=pos; i<=n; i++){
-                path.add(i);
-                DFS(res, path, i+1, n, k);
-                path.remove(path.size()-1);
+            list.add(i);
+            helper(res, list, i+1, count-1, n);
+            list.remove(list.size()-1);
         }
     }
 }
